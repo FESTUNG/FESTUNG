baec81d8a5fdc20d154af25d659862b8ec9f43db
Balthasar Reuter <balthasar.reuter@fau.de>
2015-08-18 13:16:48 +0200

    assembleMatEdgePhiPhi commented

--- assembleMatEdgePhiPhi.m
+++ assembleMatEdgePhiPhi.m
@@ -1,18 +1,117 @@
-% This file is part of FESTUNG 
-% Copyright (C) 2014 Florian Frank, Balthasar Reuter, Vadym Aizinger
-% 
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-% 
-% You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% Assembles a matrix containing integrals over interior edges of products of two 
+% basis functions.
+%
+%> @file assembleMatEdgePhiPhi.m
+%>
+%> @brief Assembles a matrix containing integrals over interior edges of 
+%>        products of two basis functions.
+%>
+%> <code>ret = assembleMatEdgePhiPhi(g, markE0Tint, refEdgePhiIntPhiInt, 
+%> refEdgePhiIntPhiExt)</code> assembles the matrix @f$\mathbf{\mathsf{S}}@f$
+%> containing integrals over edges of products of two basis functions.
+%>
+%> The matrix @f$\mathbf{\mathsf{S}} \in \mathbb{R}^{KN\times KN}@f$
+%> consists of two kinds of contributions: diagonal blocks and off-diagonal 
+%> blocks. Diagonal blocks are defined as 
+%> @f[
+%> [\mathbf{\mathsf{S}}]_{(k-1)N+i,(k-1)N+j} =
+%>  \sum_{E_{kn} \in \partial T_k \cap \mathcal{E}_D}
+%>  \frac{1}{|E_{kn}|} \int_{E_{kn}} \varphi_{ki} \varphi_{kj} \mathrm{d}s \,,
+%> @f]
+%> and off-diagonal blocks are defined as
+%> @f[
+%> [\mathbf{\mathsf{S}}]_{(k^--1)N+i,(k^+-1)N+j} =
+%>  -\frac{1}{|E_{k^-n^-}|} \int_{E_{k^-n^-}} 
+%>   \varphi_{k^-i} \varphi_{k^+j} \mathrm{d}s \,.
+%> @f]
+%> Entries in off-diagonal blocks are only non-zero for pairs of triangles
+%> @f$T_{k^-}, T_{k^+}@f$ with @f$\partial T_{k^-} \cap T_{k^+} \ne\emptyset@f$.
+%> Note that the local edge index @f$n^-@f$ is given implicitly, since 
+%> @f$\partial T_{k^-} \cap T_{k^+} \ne\emptyset@f$ consist of exactly one
+%> edge @f$E_{k^-n^-} = E_{k^+n^+}@f$.
+%> To allow for vectorization, the assembly is reformulated as
+%> @f$\mathbf{\mathsf{S}} = \mathbf{\mathsf{S}}^\mathrm{diag} + 
+%>    \mathbf{\mathsf{S}}^\mathrm{offidag}@f$ with the blocks defined as
+%> @f[
+%> \mathbf{\mathsf{S}}^\mathrm{diag} = \sum_{n=1}^3
+%>   \begin{bmatrix}
+%>     \delta_{E_{1n}\in\mathcal{E}_\Omega} &   & \\
+%>     & ~\ddots~ & \\
+%>     &          & \delta_{E_{Kn}\in\mathcal{E}_\Omega}
+%>   \end{bmatrix} \otimes [\hat{\mathbf{\mathsf{S}}}^\mathrm{diag}]_{:,:,n}\;,
+%> @f]
+%> and
+%> @f[
+%> \mathbf{\mathsf{S}}^\mathrm{offdiag} = -\sum_{n^-=1}^3\sum_{n^+=1}^3
+%>   \begin{bmatrix}
+%>     0&\delta_{E_{1n^-} = E_{1n^+}}&\dots&\dots&\delta_{E_{1n^-}=E_{Kn^+}} \\
+%>     \delta_{E_{2n^-} = E_{1n^+}}&0&\ddots& &\vdots \\
+%>     \vdots & \ddots & \ddots & \ddots & \vdots \\
+%>     \vdots & & \ddots & 0 & \delta_{E_{(K-1)n^-}=E_{Kn^+}} \\
+%>     \delta_{E_{Kn^-} = E_{1n^+}}&\dots&\dots&\delta_{E_{Kn^-} = E_{(K-1)n^+}} &0
+%>   \end{bmatrix} \otimes 
+%> [\hat{\mathbf{\mathsf{S}}}^\mathrm{offdiag}]_{:,:,n^-,n^+}\;,
+%> @f]
+%> where @f$\delta_{E_{kn}\in\mathcal{E}_\Omega}, \delta_{E_{in^-}=E_{jn^+}}@f$ 
+%> denote the Kronecker delta and @f$\otimes@f$ denotes the Kronecker product.
+%>
+%> The entries of matrix 
+%> @f$\hat{\mathbf{\mathsf{S}}}^\mathrm{diag}\in\mathbb{R}^{N\times N\times3}@f$
+%> are given by
+%> @f[
+%> [\hat{\mathbf{\mathsf{S}}}^\mathrm{diag}]_{i,j,n} =
+%>   \int_0^1 \hat{\varphi}_i \circ \hat{\mathbf{\gamma}}_n(s) 
+%>   \hat{\varphi}_j\circ \hat{\mathbf{\gamma}}_n(s) \mathrm{d}s \,,
+%> @f]
+%> where the mapping @f$\hat{\mathbf{\gamma}}_n@f$ is defined in 
+%> <code>gammaMap()</code>. The entries of matrix
+%> @f$\hat{\mathbf{\mathsf{S}}}^\mathrm{offdiag} \in 
+%>    \mathbb{R}^{N\times N\times 3\times 3}@f$ are defined as
+%> @f[
+%> [\hat{\mathbf{\mathsf{S}}}^\mathrm{offdiag}]_{i,j,n^-,n^+} =
+%>   \int_0^1 \hat{\varphi}_i \circ \hat{\mathbf{\gamma}}_{n^-}(s) 
+%>   \hat{\varphi}_j\circ \hat{\mathbf{\vartheta}}_{n^-n^+} \circ
+%>   \hat{\mathbf{\gamma}}_{n^-}(s) \mathrm{d}s \,,
+%> @f]
+%> with the mapping @f$\hat{\mathbf{\vartheta}}_{n^-n^+}@f$ as described in
+%> <code>theta()</code>.
+%>
+%> @param  g          The lists describing the geometric and topological 
+%>                    properties of a triangulation (see 
+%>                    <code>generateGridData()</code>) 
+%>                    @f$[1 \times 1 \text{ struct}]@f$
+%> @param  markE0Tint <code>logical</code> arrays that mark each triangles
+%>                    (interior) edges on which the matrix blocks should be
+%>                    assembled @f$[K \times 3]@f$
+%> @param refEdgePhiIntPhiInt  Local matrix 
+%>                    @f$\hat{\mathbf{\mathsf{S}}}^\text{diag}@f$ as provided
+%>                    by <code>integrateRefEdgePhiIntPhiInt()</code>.
+%>                    @f$[N \times N \times 3]@f$
+%> @param refEdgePhiIntPhiExt Local matrix 
+%>                    @f$\hat{\mathbf{\mathsf{S}}}^\text{diag}@f$ as provided
+%>                    by <code>integrateRefEdgePhiIntPhiExt()</code>.
+%>                    @f$[N \times N \times 3 \times 3@f$
+%> @retval ret        The assembled matrix @f$[KN \times KN]@f$
+%>
+%> This file is part of FESTUNG
+%>
+%> @copyright 2014-2015 Florian Frank, Balthasar Reuter, Vadym Aizinger
+%> 
+%> @par License
+%> @parblock
+%> This program is free software: you can redistribute it and/or modify
+%> it under the terms of the GNU General Public License as published by
+%> the Free Software Foundation, either version 3 of the License, or
+%> (at your option) any later version.
+%>
+%> This program is distributed in the hope that it will be useful,
+%> but WITHOUT ANY WARRANTY; without even the implied warranty of
+%> MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%> GNU General Public License for more details.
+%>
+%> You should have received a copy of the GNU General Public License
+%> along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%> @endparblock
 %
 function ret = assembleMatEdgePhiPhi(g, markE0Tint, refEdgePhiIntPhiInt, refEdgePhiIntPhiExt)
 K = g.numT;  N = size(refEdgePhiIntPhiInt, 1);
