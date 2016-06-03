8d17998f6f935a032d67b24c06d9ca23d9d218d4
Balthasar Reuter <balthasar.reuter@fau.de>
2016-06-03 13:43:18 +0200

    Applied changes to minimum value correction

--- correctMinValueExceedanceDisc.m
+++ correctMinValueExceedanceDisc.m
@@ -12,30 +12,30 @@ end % if
 [K, N] = size(dataDisc);
 p = (sqrt(8*N+1)-3)/2;
 if p == 0
-  HLagrange = dataDisc * phi(1,1/3,1/3);
+  dataLagr = dataDisc * phi(1,1/3,1/3);
 else
-  HLagrange = zeros(K,3);
+  dataLagr = zeros(K,3);
   for i = 1:N
-    HLagrange = HLagrange + dataDisc(:,i) * phi(i, [0, 1, 0], [0, 0, 1]);
+    dataLagr = dataLagr + dataDisc(:,i) * phi(i, [0, 1, 0], [0, 0, 1]);
   end % for
 end % if
-corr = max(minTol - HLagrange, 0);
-normCorr = norm(corr);
-if p == 0
-  if ~isequal(corr, zeros(K,1))
-    warning([ 'Correction of variable c1 in vertices is needed at timestep' ' ' num2str(nStep) '. Norm of correction values:' ' ' num2str(normCorr) ] );
-		if normCorr > correctionTolerance
-			error('The corrections needed for variable c1 excced the maximum corrections allowed.')
-		end % if
+corr = max(minTol - dataLagr, 0);
+maxCorr = max(abs(corr(:)));
+if maxCorr > correctionTolerance
+  [indx, indy] = find(abs(corr) == maxCorr, 1);
+  error([ 'Unknown at local vertex ' num2str(indy) ' of element ' num2str(indx) ...
+          ' in step ' num2str(nStep) ' is ' num2str(abs(dataLagr(indx, indy))) ...
+          ' (below the minimum tolerance of ' num2str(correctionTolerance) ').' ]);
+end % if
+if any(corr)
+  [indx, indy] = find(abs(corr) == maxCorr, 1);
+  warning([ 'A maximum value of ' num2str(maxCorr) ...
+            ' had to be added to unknown at local vertex ' num2str(indy) ...
+            ' of element ' num2str(indx) ' in step ' num2str(nStep) '.' ]);
+  if p == 0
     dataDisc = dataDisc + corr / phi(1,1/3,1/3);
-  end %  if
-else
-  if ~isequal(corr, zeros(K,3))
-    warning([ 'Correction of variable c1 in vertices is needed at timestep' ' ' num2str(nStep) '. Norm of correction values:' ' ' num2str(normCorr) ] );
-    if normCorr > correctionTolerance
-			error('The corrections needed for variable c1 excced the maximum corrections allowed.')
-    end % if
-		dataDisc(:,1:3) = dataDisc(:,1:3) + corr / corrSys;
+  else
+    dataDisc(:,1:3) = dataDisc(:,1:3) + corr / corrSys;
   end % if
 end % if
 end % function
