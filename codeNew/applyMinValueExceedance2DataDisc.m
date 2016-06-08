aff39a75fa6954aa6d3d0b508e9d930c4b624fdd
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-06-08 17:19:18 +0200

    Changed applyMinValueExceedence framework to work on xi instead of H.

--- codeNew/applyMinValueExceedance2DataDisc.m
+++ codeNew/applyMinValueExceedance2DataDisc.m
@@ -1,41 +1,37 @@
 function dataDisc = applyMinValueExceedance2DataDisc(g, dataDisc, corrSys, nStep, minTol, correctionTolerance)
-if nargin < 3
+if nargin < 4
 	nStep = [];
 end % if
-if nargin < 4
+if nargin < 5
 	minTol = 0;
 end % if
-if nargin < 5
+if nargin < 6
 	correctionTolerance = Inf;
 end % if
 [K, N] = size(dataDisc);
 p = (sqrt(8*N+1)-3)/2;
-if p == 0
-  dataLagrange = dataDisc * phi(1,1/3,1/3);
-else
-  dataLagrange = zeros(K,3);
-  for i = 1:N
-    dataLagrange = dataLagrange + dataDisc(:,i) * phi(i, [0, 1, 0], [0, 0, 1]);
-  end % for
-end % if
-corr = max(minTol - dataLagrange, 0);
-maxCorr = max(abs(corr(:)));
+dataV0T = zeros(K,3);
+for i = 1:N
+  dataV0T = dataV0T + dataDisc(:,i) * phi(i, [0, 1, 0], [0, 0, 1]);
+end % for
+corr = max(minTol - dataV0T, 0);
+maxCorr = max(corr(:));
 if maxCorr > correctionTolerance
-  [indx, indy] = find(abs(corr) == maxCorr, 1);
-  error([ 'Unknown at node ' num2str(g.V0T(indx, indy)) ' and step ' num2str(nStep) ' is ' num2str(dataLagrange(indx, indy)) ...
+  [indx, indy] = find(corr == maxCorr, 1);
+  error([ 'Unknown at node ' num2str(g.V0T(indx, indy)) ' and step ' num2str(nStep) ' is ' num2str(dataV0T(indx, indy)) ...
           ', which is below the tolerated minimal value.' ]);
 end % if
 if p == 0
-  if ~isequal(corr, zeros(K,1))
-    [indx, indy] = find(abs(corr) == maxCorr, 1);
-    dataDisc = dataDisc + corr / phi(1,1/3,1/3);
+  if ~isequal(corr, zeros(K,3))
+    [indx, indy] = find(corr == maxCorr, 1);
+    dataDisc = dataDisc + max(corr, [], 2) / phi(1, 1/3, 1/3);
     warning([ 'A maximal value of ' num2str(maxCorr) ' had to be added to unknown at node ' num2str(g.V0T(indx, indy)) ...
               ' and step ' num2str(nStep) '.' ]);
   end %  if
 else
   if ~isequal(corr, zeros(K,3))
-    [indx, indy] = find(abs(corr) == maxCorr, 1);
-    dataDisc(:,1:3) = dataDisc(:,1:3) + corr / corrSys;
+    [indx, indy] = find(corr == maxCorr, 1);
+    dataDisc(:,1:3) = dataDisc(:,1:3) + corr / corrSys; % TODO evtl speichern
     warning([ 'A maximal value of ' num2str(maxCorr) ' had to be added to unknown at node ' num2str(g.V0T(indx, indy)) ...
               ' and step ' num2str(nStep) '.' ]);
   end % if
