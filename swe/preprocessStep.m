444dd0db7bfa34b76083617847f834e4c5e6e635
Balthasar Reuter <balthasar.reuter@fau.de>
2016-06-14 18:28:45 +0200

    jump stored directly

--- swe/preprocessStep.m
+++ swe/preprocessStep.m
@@ -79,7 +79,7 @@ qOrd2D = max(2*p,1); [~, ~, W] = quadRule2D(qOrd2D); numQuad2D = length(W);
 cQ0T = cell(3,1); % cDisc in quadrature points of triangles
 cQ0E0Tint = cell(3,3); % cDisc in interior quad points of edges
 cQ0E0Text = cell(3,3,3); % cDisc in exterior quad points of edges
-cQ0E0TE0T = cell(3,3,3); % cDisc in quad points of edge on neighboring element
+jumpQ0E0TE0T = cell(3,3,3); % cDisc in quad points of edge on neighboring element
 
 for i = 1 : 3
   cQ0T{i} = reshape(pd.basesOnQuad.phi2D{qOrd2D} * pd.cDisc(:,:,i).', numQuad2D * K, 1);
@@ -88,7 +88,8 @@ for i = 1 : 3
     for np = 1 : 3
       cDiscThetaPhi = pd.basesOnQuad.thetaPhi1D{qOrd1D}(:,:,nn,np) * pd.cDisc(:,:,i).';
       cQ0E0Text{i,nn,np} = reshape(cDiscThetaPhi, numQuad1D * K, 1);
-      cQ0E0TE0T{i,nn,np} = reshape(cDiscThetaPhi * pd.g.markE0TE0T{nn,np}.', numQuad1D * K, 1);
+      jumpQ0E0TE0T{i,nn,np} = cQ0E0Tint{i,nn} - ...
+                              reshape(cDiscThetaPhi * pd.g.markE0TE0T{nn,np}.', numQuad1D * K, 1);
     end % for
   end % for
 end % for
@@ -98,8 +99,6 @@ hQ0T = cQ0T{1} - pd.zbQ0T; % water height (xi - zb) in quadrature points of tria
 hQ0E0Tint = cellfun(@minus, cQ0E0Tint(1,:).', pd.zbQ0E0Tint, 'UniformOutput', false);
 % water height (xi - zb) in exterior quad points of edges
 hQ0E0Text = cellfun(@minus, squeeze(cQ0E0Text(1,:,:)), pd.zbQ0E0Text, 'UniformOutput', false);
-% water height (xi - zb) in quad points of edge on neighboring element
-hQ0E0TE0T = cellfun(@minus, squeeze(cQ0E0TE0T(1,:,:)), pd.zbQ0E0TE0T, 'UniformOutput', false);
 
 markQ0E0TbdrL = cell(3,1);
 markQ0E0TbdrOS = cell(3,1);
@@ -225,9 +224,9 @@ for nn = 1 : 3
           
         lambda = computeLaxFriedrichsCoefficient(cRoePikeQ0E0T, pd.g.nuQ0E0T(nn,:), pd.gConst);
         pd.riemannTerms = pd.riemannTerms + ...
-          [ pd.globV{nn,np} * (lambda .* (cQ0E0Tint{1,nn} - cQ0E0TE0T{1,nn,np})) ; ...
-            pd.globV{nn,np} * (lambda .* (cQ0E0Tint{2,nn} - cQ0E0TE0T{2,nn,np})) ; ...
-            pd.globV{nn,np} * (lambda .* (cQ0E0Tint{3,nn} - cQ0E0TE0T{3,nn,np})) ];
+          [ pd.globV{nn,np} * (lambda .* jumpQ0E0TE0T{1,nn,np}) ; ...
+            pd.globV{nn,np} * (lambda .* jumpQ0E0TE0T{2,nn,np}) ; ...
+            pd.globV{nn,np} * (lambda .* jumpQ0E0TE0T{3,nn,np}) ];
 
       case 'Roe'
         error('not implemented')
