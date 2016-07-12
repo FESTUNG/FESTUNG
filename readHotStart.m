fbacc7a54f14150db65cd42f09d5919e91ead551
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-07-12 16:55:54 +0200

    Added and tested routine for grid refinement, working in our framework.

--- readHotStart.m
+++ readHotStart.m
@@ -1,5 +1,4 @@
 function ret = readHotStart(g, file)
-% cd('output');
 K = g.numT;
 fileID = fopen(file);
 ret = cell2mat(textscan(fileID, '%f'));
@@ -7,5 +6,4 @@ N = length(ret) / K;
 assert(ismember(N, [1 3 6 10 15]), 'Hot start file has wrong number of values.');
 ret = reshape(ret, N, K).';
 fclose(fileID);
-% cd ..
 end % function
