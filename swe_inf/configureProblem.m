92c03f14245a569072de9178c399f9688238b6ff
Balthasar Reuter <balthasar.reuter@fau.de>
2016-07-26 13:38:19 +0200

    Additional testcase added. Verified correctness compared to code version before change to coupling framework

--- swe_inf/configureProblem.m
+++ swe_inf/configureProblem.m
@@ -25,7 +25,7 @@ pd.isRiemOS = true; % Riemann solver type on open sea boundary ('Lax-Friedrichs'
 pd.typeBdrL = 'riemann'; % Flux type on land boundary ('reflected', 'natural', or 'riemann')
 
 %% Testcase
-pd = configureAnalyticalTest(pd);
+pd = configureAnalyticalTest2(pd);
 
 %% Waitbar
 pd.isWaitbar = false;
@@ -88,6 +88,104 @@ pd.zb_yCont = @(x1,x2,t)    zeros(size(x1));
 pd.h_tCont  = @(x1,x2,t)    zeros(size(x1));
 pd.h_xCont  = @(x1,x2,t)    zeros(size(x1));
 pd.h_yCont  = @(x1,x2,t)    zeros(size(x1));
+
+% Right hand side functions (derived from analytical solution)
+
+      
+pd.f0Cont = @(x1,x2,t) pd.h_tCont(x1,x2,t) + ...
+                      (pd.u_xCont(x1,x2,t) + pd.v_yCont(x1,x2,t)) .* pd.hCont(x1,x2,t) + ...
+                       pd.uCont(x1,x2,t) .* pd.h_xCont(x1,x2,t) + ...
+                       pd.vCont(x1,x2,t) .* pd.h_yCont(x1,x2,t);
+pd.f1Cont = @(x1,x2,t) pd.u_tCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       pd.uCont(x1,x2,t) .* pd.h_tCont(x1,x2,t) + ...
+                       ( 2 * pd.uCont(x1,x2,t) .* pd.u_xCont(x1,x2,t) + pd.gConst * pd.h_xCont(x1,x2,t) ) .* pd.hCont(x1,x2,t) + ... 
+                       pd.uCont(x1,x2,t) .* pd.uCont(x1,x2,t) .* pd.h_xCont(x1,x2,t) + ...
+                       pd.uCont(x1,x2,t) .* pd.v_yCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ... 
+                       pd.uCont(x1,x2,t) .* pd.vCont(x1,x2,t) .* pd.h_yCont(x1,x2,t) + ...
+                       pd.u_yCont(x1,x2,t) .* pd.vCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       pd.gConst * pd.zb_xCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       pd.bottomFrictionCoef * sqrt( pd.uCont(x1,x2,t) .* pd.uCont(x1,x2,t) + pd.vCont(x1,x2,t) .* pd.vCont(x1,x2,t) ) .* pd.uCont(x1,x2,t) - ...
+                       pd.fcCont(x1,x2) .* pd.vCont(x1,x2,t) .* pd.hCont(x1,x2,t);
+pd.f2Cont = @(x1,x2,t) pd.v_tCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       pd.vCont(x1,x2,t) .* pd.h_tCont(x1,x2,t) + ...
+                       pd.u_xCont(x1,x2,t) .* pd.vCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       pd.uCont(x1,x2,t) .* pd.vCont(x1,x2,t) .* pd.h_xCont(x1,x2,t) + ...
+                       pd.uCont(x1,x2,t) .* pd.v_xCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       2 * pd.vCont(x1,x2,t) .* pd.v_yCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       pd.vCont(x1,x2,t) .* pd.vCont(x1,x2,t) .* pd.h_yCont(x1,x2,t) + ... 
+                       pd.gConst * pd.h_yCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       pd.gConst * pd.zb_yCont(x1,x2,t) .* pd.hCont(x1,x2,t) + ...
+                       pd.bottomFrictionCoef * sqrt( pd.uCont(x1,x2,t) .* pd.uCont(x1,x2,t) + pd.vCont(x1,x2,t) .* pd.vCont(x1,x2,t) ) .* pd.vCont(x1,x2,t) + ...
+                       pd.fcCont(x1,x2) .* pd.uCont(x1,x2,t) .* pd.hCont(x1,x2,t);
+                    
+                     
+% Boundary conditions
+pd.xiOSCont = pd.xiCont;
+pd.isRivCont = false;
+
+%zb is linear in finite and constant in infinite direction
+%OS-Bdr at x1 = 100
+pd.zbOSAlg = @(x1,x2) pd.zbCont(100,x2);
+pd.HOSAlg  = @(x1,x2,t) pd.xiCont(x1,x2,t) - pd.zbOSAlg(x1,x2);
+
+%height >= 0
+pd.minTol = 0.0001;
+end % function
+
+function pd = configureAnalyticalTest2(pd)
+pd.isSolutionAvail = true;
+pd.isRhsAvail = true;
+pd.isTidalDomain = false;
+
+% Overwrite grid parameters
+pd.gridSource = 'hierarchical';
+pd.isSpherical = false; 
+pd.hmax = 30; % Maximum element size of initial grid
+pd.refinement = 0;  % Grid refinement level
+
+% Overwrite time-stepping parameters
+pd.t0 = 0; % Start time of simulation
+pd.tEnd = 1; % End time of simulation
+pd.numSteps = 1; % Number of time steps
+
+pd.isAdaptiveTimestep = false; % Use adaptive timestep width
+pd.dt = (pd.tEnd - pd.t0) / pd.numSteps;
+
+pd.isSteadyState = false; % End simulation upon convergence
+
+% Solution parameters
+height = 1;
+A = 0.01;
+B = 0.1;
+pd.gConst = 9.81;
+
+pd.isBottomFrictionNonlinear = true; % NOLIBF
+pd.isBottomFrictionVarying = false; % NWP
+pd.bottomFrictionCoef = 1;
+
+% Ramping function, bathymetry, and Coriolis coefficient
+pd.isRamp = false;
+pd.zbCont = @(x1,x2) -height * (6 - A * x2);
+pd.fcCont = @(x1,x2) zeros(size(x1));
+
+% Analytical solution
+pd.xiCont = @(x1,x2,t) B * ( cos(A*pi*(x2)) );
+pd.uCont = @(x1,x2,t) A*x1;
+pd.vCont = @(x1,x2,t) B*sin(A*pi*x2);
+
+% Auxiliary functions (derivatives etc.)
+pd.hCont = @(x1,x2,t) pd.xiCont(x1,x2,t) - pd.zbCont(x1,x2);
+pd.u_tCont  = @(x1,x2,t)    zeros(size(x1));
+pd.u_xCont  = @(x1,x2,t)   A*ones(size(x1));
+pd.u_yCont  = @(x1,x2,t)    zeros(size(x1));
+pd.v_tCont  = @(x1,x2,t)    zeros(size(x1));
+pd.v_xCont  = @(x1,x2,t)    zeros(size(x1));
+pd.v_yCont  = @(x1,x2,t)    A*B*pi*cos(A*pi*x2);
+pd.zb_xCont = @(x1,x2,t)    zeros(size(x1));
+pd.zb_yCont = @(x1,x2,t)    A * height*ones(size(x2));
+pd.h_tCont  = @(x1,x2,t)    zeros(size(x1));
+pd.h_xCont  = @(x1,x2,t)    zeros(size(x1));
+pd.h_yCont  = @(x1,x2,t)    -A*B*pi * sin(A*pi*(x2)) - A*height*ones(size(x2));
 
 % Right hand side functions (derived from analytical solution)
 
