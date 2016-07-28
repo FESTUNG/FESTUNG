72c09813261f208e778ee584623115bb879b0d2c
Balthasar Reuter <balthasar.reuter@fau.de>
2016-07-28 14:16:32 +0200

    Usage of setdefault() and execin() in configure routines

--- configureProblem.m
+++ configureProblem.m
@@ -1,18 +1,17 @@
 function problemData = configureProblem(problemData)
 
+% Configuration for shallow water solver
 problemData.sweData = struct;
-h = getFunctionHandle('swe/configureProblem');
-problemData.sweData = h(problemData.sweData);
 problemData.sweData.isCoupling = true;
 
-problemData.transportData = struct;
+problemData.sweData = execin('swe/configureProblem', problemData.sweData);
 
+% Configuration for transport solver
+problemData.transportData = struct;
 problemData.transportData.ordRK = problemData.sweData.schemeOrder; % as of now both models have to use the same RK method
 problemData.transportData.tEnd = problemData.sweData.tEnd;
 problemData.transportData.numSteps = problemData.sweData.numSteps; % as of now both models have to use the same time step
-
 problemData.transportData.isVisGrid = false; % visualization of grid
 
-h = getFunctionHandle('transport/configureProblem');
-problemData.transportData = h(problemData.transportData);
+problemData.transportData = execin('transport/configureProblem', problemData.transportData);
 end % function
