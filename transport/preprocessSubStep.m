a28ca0cafcec18f9d1c92c12727756c58e85d791
Balthasar Reuter <balthasar.reuter@fau.de>
2016-07-28 14:34:39 +0200

    Use RK solution instead of solution from old timestep for evaluation in quadrature points

--- transport/preprocessSubStep.m
+++ transport/preprocessSubStep.m
@@ -1,14 +1,20 @@
 function problemData = preprocessSubStep(problemData, ~, nSubStep)
-qOrd2D = max(2* problemData.p,1);
+K = problemData.K;
+N = problemData.N;
+p = problemData.p;
 
-if ~isfield(problemData, 'u1Disc') && ~isfield(problemData, 'u2Disc') && ~isfield(problemData, 'vNormalOnQuadEdge')
+% Project velocity to DG space, if not given
+if ~all(isfield(problemData, {'u1Disc', 'u2Disc', 'vNormalOnQuadEdge'}))
   problemData.u1Disc = projectFuncCont2DataDisc(problemData.g, @(x1,x2) problemData.u1Cont(problemData.timeLvls(nSubStep),x1,x2), ...
-                                                              2*problemData.p, problemData.hatM, problemData.basesOnQuad);
+                                                2*p, problemData.hatM, problemData.basesOnQuad);
   problemData.u2Disc = projectFuncCont2DataDisc(problemData.g, @(x1,x2) problemData.u2Cont(problemData.timeLvls(nSubStep),x1,x2), ...
-                                                              2*problemData.p, problemData.hatM, problemData.basesOnQuad);
+                                                2*p, problemData.hatM, problemData.basesOnQuad);
   % Evaluate normal velocity in quadrature points of edges
   problemData.vNormalOnQuadEdge = computeFuncContNuOnQuadEdge(problemData.g, @(x1,x2) problemData.u1Cont(problemData.timeLvls(nSubStep),x1,x2), ...
-                                                              @(x1,x2) problemData.u2Cont(problemData.timeLvls(nSubStep),x1,x2), 2*problemData.p+1);
+                                                              @(x1,x2) problemData.u2Cont(problemData.timeLvls(nSubStep),x1,x2), 2*p+1);
 end % if
-problemData.cQ0T = cellfun(@(c) c * problemData.basesOnQuad.phi2D{qOrd2D}.', problemData.cDisc, 'UniformOutput', false);
+
+% Evaluate cDisc in all quadrature points
+qOrd2D = max(2*p,1);
+problemData.cQ0T = cellfun(@(c) (problemData.basesOnQuad.phi2D{qOrd2D} * reshape(c, N, K)).', problemData.cDiscRK, 'UniformOutput', false);
 end % function
