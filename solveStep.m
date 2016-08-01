e19101cc0ff19bbf762f8203550d864f7c9e44ca
Balthasar Reuter <balthasar.reuter@fau.de>
2016-08-01 13:17:02 +0200

    Formatting and small corrections

--- solveStep.m
+++ solveStep.m
@@ -1,10 +1,12 @@
 function problemData = solveStep(problemData, nStep)
-K = problemData.transportData.K;
-N = problemData.transportData.N;
 
 % Obtain Runge-Kutta rule % TODO: change to allow changes in time increment
 [problemData.timeLvls, problemData.omega] = rungeKuttaSSP(problemData.sweData.schemeOrder, problemData.sweData.dt, ...
                                                           (nStep - 1) * problemData.sweData.dt);
+
+% Initialize time stepping for SWE problem
+K = problemData.sweData.K;
+N = problemData.sweData.N;
 
 problemData.sweData.tLvls = problemData.timeLvls;
 problemData.sweData.omega = problemData.omega;
@@ -14,12 +16,17 @@ problemData.sweData.cDiscRK0 = [ reshape(problemData.sweData.cDisc(:,:,1).', K*N
                                  reshape(problemData.sweData.cDisc(:,:,3).', K*N, 1) ];
 problemData.sweData.cDiscRK = problemData.sweData.cDiscRK0;
 
+% Initialize time stepping for transport problem
+K = problemData.transportData.K;
+N = problemData.transportData.N;
+
 problemData.transportData.timeLvls = problemData.timeLvls;
 problemData.transportData.omega = problemData.omega;
 
 problemData.transportData.cDiscRK0 = cellfun(@(c) reshape(c.', [K*N,1]), problemData.transportData.cDisc, 'UniformOutput', false);
 problemData.transportData.cDiscRK = problemData.transportData.cDiscRK0;
 
+% Perform RK stepping
 problemData.isSubSteppingFinished = false;
 problemData = iterateSubSteps(problemData, nStep);
 end % function
