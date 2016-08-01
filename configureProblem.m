a1bd9b4aeef052315851b0578b8e44dc346e7c50
Balthasar Reuter <balthasar.reuter@fau.de>
2016-08-01 13:17:47 +0200

    Polynomial order choice outside of configureProblem possible

--- configureProblem.m
+++ configureProblem.m
@@ -1,13 +1,18 @@
 function problemData = configureProblem(problemData)
+% Polynomial approximation order
+p = 2;
 
 % Configuration for shallow water solver
 problemData.sweData = struct;
 problemData.sweData.isCoupling = true;
+problemData.sweData.p = p;
 
 problemData.sweData = execin('swe/configureProblem', problemData.sweData);
+% problemData.sweData.schemeOrder = 1;
 
 % Configuration for transport solver
 problemData.transportData = struct;
+problemData.transportData.p = p;
 problemData.transportData.ordRK = problemData.sweData.schemeOrder; % as of now both models have to use the same RK method
 problemData.transportData.tEnd = problemData.sweData.tEnd;
 problemData.transportData.numSteps = problemData.sweData.numSteps; % as of now both models have to use the same time step
