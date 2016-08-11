d9f0a1e11b915e30fad6efac4134a45fa5a2c085
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-08-11 13:00:00 +0200

    Changes in config files again. Fixed bug of inconsistency of hCont, which has its arguments as in swe, i.e. (x1,x2,t) instead of (t,x1,x2). Note: This should be unified to be consistent throughout FESTUNG. Minor changes regarding initial conditions and output.

--- postprocessProblem.m
+++ postprocessProblem.m
@@ -1,11 +1,12 @@
 function problemData = postprocessProblem(problemData)
 problemData.errors = zeros(problemData.numSpecies,1);
 for species = 1:problemData.numSpecies
-  if problemData.isVisSol{species}
-    cLagrange = projectDataDisc2DataLagr(problemData.cDisc{species});
-    visualizeDataLagr(problemData.g, cLagrange, ['cH_' num2str(species) '_h'], problemData.outputBasename{species}, ...
-                      problemData.numSteps, problemData.outputTypes{species});
-  end % if
+%   %% Visualization
+%   if problemData.isVisSol{species}
+%     cLagrange = projectDataDisc2DataLagr(problemData.cDisc{species});
+%     visualizeDataLagr(problemData.g, cLagrange, ['cH_' num2str(species) '_h'], problemData.outputBasename{species}, ...
+%                       problemData.numSteps, problemData.outputTypes{species});
+%   end % if
   %% Error evaluation
 %   fprintf('L2 error w.r.t. the initial condition: %g\n', ...
   problemData.errors(species) = computeL2Error(problemData.g, problemData.cDisc{species}, @(x1,x2) problemData.solCont{species}(problemData.tEnd,x1,x2), 2*problemData.p, problemData.basesOnQuad);%);
