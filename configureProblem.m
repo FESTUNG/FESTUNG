5119591774887e432259bf686fc65e40e4de3067
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-08-12 19:11:14 +0200

    Rescaled and made minor changes to convergence case. Output is different in some ways. Only numerical change is computation of massFluxQ0T in swe which has to take into account domain boundary conditions for fluxes on boundary. So far this is only done for river boundaries with Riemann solver usage. Convergence of shallow water solution seems to work, convergence of concentrations does not, yet. Ignore changes in advection.

--- configureProblem.m
+++ configureProblem.m
@@ -3,21 +3,18 @@ function problemData = configureProblem(problemData)
 %% Configuration to use: 
 % - 'rotation' calls configureRotation()
 % - 'analytical' calls configureAnalyticalTest()
-problemData = setdefault(problemData, 'configSource', 'rotation');
+problemData = setdefault(problemData, 'configSource', 'analytical');
 
 %% Parameters. 
 % Set default values if they are not yet available in problemData
-problemData = setdefault(problemData, 'numSpecies', 3);  % number of transported species
+problemData = setdefault(problemData, 'numSpecies', 3);  % number of transported species % TODO
 problemData = setdefault(problemData, 'p'         , 0);  % local polynomial degree (TODO: allow different approximation orders for each species)
-problemData = setdefault(problemData, 'hmax'      , 2^-6);  % maximum edge length of triangle
 problemData = setdefault(problemData, 'ordRK'     , min(problemData.p+1,3));  % order of Runge Kutta time stepper
 problemData = setdefault(problemData, 'isVisGrid' , false);  % visualization of grid
 
 %% Parameter check.
 assert(problemData.p >= 0 && problemData.p <= 4, 'Polynomial order must be zero to four.')
 assert(problemData.ordRK >= 1 && problemData.ordRK <= 3, 'Order of Runge Kutta must be zero to three.')
-assert(problemData.hmax > 0, 'Maximum edge length must be positive.')
-assert(problemData.numSteps > 0, 'Number of time steps must be positive.')
 
 problemData.isVisSol = cell(problemData.numSpecies,1);
 problemData.isSlopeLim = cell(problemData.numSpecies,1);
@@ -34,10 +31,14 @@ problemData.reactions = cell(problemData.numSpecies,1);
 %% Simulation scenario specific parameters
 switch problemData.configSource
   case 'rotation'
-    problemData = setdefault(problemData, 'numSteps'  , 314);  % number of time steps
+    problemData.isSolutionAvailable = false;
+    problemData = setdefault(problemData, 'hmax'      , 2^-6);  % maximum edge length of triangle
+    problemData = setdefault(problemData, 'numSteps'  , 100);  % number of time steps
     problemData = setdefault(problemData, 'tEnd'      , (problemData.numSteps/3142)*2*pi);  % end time
     problemData = configureRotation(problemData);
   case 'analytical'
+    problemData.isSolutionAvailable = true;
+    problemData = setdefault(problemData, 'hmax'      , 1);  % maximum edge length of triangle
     problemData = setdefault(problemData, 'numSteps'  , 100);  % number of time steps
     problemData = setdefault(problemData, 'tEnd'      , 1);  % end time
     problemData = configureAnalyticalTest(problemData);
@@ -45,12 +46,15 @@ switch problemData.configSource
     error('Invalid config source.')
 end % switch
 
+assert(problemData.hmax > 0, 'Maximum edge length must be positive.')
+assert(problemData.numSteps > 0, 'Number of time steps must be positive.')
+
 end % function
 
 %% LeVeque's solid body rotation
 function problemData = configureRotation(problemData)
 
-problemData = setdefault(problemData, 'hCont', @(x1,x2,t) 0.002*(x1==x1)); % TODO order of arguments
+problemData = setdefault(problemData, 'hCont', @(x1,x2,t) x1==x1); % TODO order of arguments
 problemData = setdefault(problemData, 'uCont', @(t,x1,x2) 0.5 - x2);
 problemData = setdefault(problemData, 'vCont', @(t,x1,x2) x1 - 0.5);
 problemData = setdefault(problemData, 'uHCont', @(t,x1,x2) problemData.hCont(x1,x2,t) .* problemData.uCont(t,x1,x2));
@@ -79,9 +83,9 @@ for species = 1:problemData.numSpecies
   problemData.gNCont{species} = @(t,x1,x2) zeros(size(x1));
 end % for
 
-problemData.reactions{1} = @(t,x1,x2,c) 0*x1;
-problemData.reactions{2} = @(t,x1,x2,c) 0*x1;
-problemData.reactions{3} = @(t,x1,x2,c) 0*x1;
+problemData.reactions{1} = @(t,x1,x2,c,cH) 0*x1;
+problemData.reactions{2} = @(t,x1,x2,c,cH) 0*x1;
+problemData.reactions{3} = @(t,x1,x2,c,cH) 0*x1;
 
 problemData.fCont{1} = @(t,x1,x2) 0*x1;
 problemData.fCont{2} = @(t,x1,x2) 0*x1;
@@ -97,22 +101,23 @@ B = 0.01;
 C = 0.01;
 height = 0.025;
 
-problemData = setdefault(problemData, 'hCont', @(x1,x2,t) C*(sin(0.5*pi*(x1-t)) + sin(0.5*pi*(x2-t))) - (height*(x1+x2) - 0.1));
-problemData = setdefault(problemData, 'h_tCont', @(t,x1,x2) -0.5*pi*C*(cos(0.5*pi*(x1-t)) + cos(0.5*pi*(x2-t))));
-problemData = setdefault(problemData, 'h_xCont', @(t,x1,x2) 0.5*pi*C*cos(0.5*pi*(x1-t)) - height);
-problemData = setdefault(problemData, 'h_yCont', @(t,x1,x2) 0.5*pi*C*cos(0.5*pi*(x2-t)) - height);
-problemData = setdefault(problemData, 'uCont', @(t,x1,x2) A*sin(0.5*pi*(x2-t)).*sin(pi*x1));
-problemData = setdefault(problemData, 'u_xCont', @(t,x1,x2) pi*A*sin(0.5*pi*(x2-t)).*cos(pi*x1));
-problemData = setdefault(problemData, 'vCont', @(t,x1,x2) B*sin(0.5*pi*(x1-t)).*sin(pi*x2));
-problemData = setdefault(problemData, 'v_yCont', @(t,x1,x2) pi*B*sin(0.5*pi*(x1-t)).*cos(pi*x2));
-problemData = setdefault(problemData, 'uHCont', @(t,x1,x2) problemData.hCont(x1,x2,t) .* problemData.uCont(t,x1,x2));
-problemData = setdefault(problemData, 'vHCont', @(t,x1,x2) problemData.hCont(x1,x2,t) .* problemData.vCont(t,x1,x2));
+% TODO
+% problemData = setdefault(problemData, 'hCont', @(x1,x2,t) C*(sin(0.5*pi*(x1-t)) + sin(0.5*pi*(x2-t))) - (height*(x1+x2) - 0.1));
+% problemData = setdefault(problemData, 'h_tCont', @(t,x1,x2) -0.5*pi*C*(cos(0.5*pi*(x1-t)) + cos(0.5*pi*(x2-t))));
+% problemData = setdefault(problemData, 'h_xCont', @(t,x1,x2) 0.5*pi*C*cos(0.5*pi*(x1-t)) - height);
+% problemData = setdefault(problemData, 'h_yCont', @(t,x1,x2) 0.5*pi*C*cos(0.5*pi*(x2-t)) - height);
+% problemData = setdefault(problemData, 'uCont', @(t,x1,x2) A*sin(0.5*pi*(x2-t)).*sin(pi*x1));
+% problemData = setdefault(problemData, 'u_xCont', @(t,x1,x2) pi*A*sin(0.5*pi*(x2-t)).*cos(pi*x1));
+% problemData = setdefault(problemData, 'vCont', @(t,x1,x2) B*sin(0.5*pi*(x1-t)).*sin(pi*x2));
+% problemData = setdefault(problemData, 'v_yCont', @(t,x1,x2) pi*B*sin(0.5*pi*(x1-t)).*cos(pi*x2));
+% problemData = setdefault(problemData, 'uHCont', @(t,x1,x2) problemData.hCont(x1,x2,t) .* problemData.uCont(t,x1,x2));
+% problemData = setdefault(problemData, 'vHCont', @(t,x1,x2) problemData.hCont(x1,x2,t) .* problemData.vCont(t,x1,x2));
 
 % analytical solution
-problemData.solCont = {@(t,x1,x2) sin(x1+x2)+1, @(t,x1,x2) cos(x1+x2)+1, @(t,x1,x2) -cos(x1+x2)+1};
-sol_tCont = {@(t,x1,x2) 0*x1, @(t,x1,x2) 0*x1, @(t,x1,x2) 0*x1};
-sol_xCont = {@(t,x1,x2) cos(x1+x2), @(t,x1,x2) -sin(x1+x2), @(t,x1,x2) sin(x1+x2)};
-sol_yCont = {@(t,x1,x2) cos(x1+x2), @(t,x1,x2) -sin(x1+x2), @(t,x1,x2) sin(x1+x2)};
+problemData.solCont = {@(t,x1,x2) sin(0.0001*(x1+x2-t))+2, @(t,x1,x2) cos(0.0001*(x1+x2-t))+2, @(t,x1,x2) -cos(0.0001*(x1+x2-t))+2};
+sol_tCont = {@(t,x1,x2) -0.0001*cos(0.0001*(x1+x2-t)), @(t,x1,x2) 0.0001*sin(0.0001*(x1+x2-t)), @(t,x1,x2) -0.0001*sin(0.0001*(x1+x2-t))};
+sol_xCont = {@(t,x1,x2) 0.0001*cos(0.0001*(x1+x2-t)), @(t,x1,x2) -0.0001*sin(0.0001*(x1+x2-t)), @(t,x1,x2) 0.0001*sin(0.0001*(x1+x2-t))};
+sol_yCont = {@(t,x1,x2) 0.0001*cos(0.0001*(x1+x2-t)), @(t,x1,x2) -0.0001*sin(0.0001*(x1+x2-t)), @(t,x1,x2) 0.0001*sin(0.0001*(x1+x2-t))};
 
 for species = 1:problemData.numSpecies
   problemData.isVisSol{species}    = true; % visualization of solution
@@ -120,7 +125,7 @@ for species = 1:problemData.numSpecies
   problemData.typeSlopeLim{species} = 'hierarch_vert'; % Type of slope limiter (linear, hierarch_vert, strict)
   
   problemData.outputFrequency{species} = problemData.numSteps/10; % no visualization of every timestep
-  problemData.outputBasename{species}  = ['output' filesep 'solution_' num2str(species) '_' problemData.typeSlopeLim{species}]; % Basename of output files
+  problemData.outputBasename{species}  = ['output' filesep 'solution_' num2str(species)]; % Basename of output files
   problemData.outputTypes{species}     = cellstr('vtk'); % solution output file types
   
   %% Parameter check.
@@ -135,10 +140,10 @@ end % for
 % NPZ model
 % parameters (Notation as in paper)
 I0 = 1;
-Vm = 1;
+Vm = 0.0000001;
 ks = 1;
-Rm = 1;
-ep = 1;
+Rm = 0.0000001;
+ep = 0.0000001;
 gamma = 0.5;
 I = @(t,x1,x2) (x1==x1);
 f = @(t,x1,x2) I(t,x1,x2) /I0; % linear response
@@ -154,23 +159,23 @@ problemData.reactions{3} = @(t,x1,x2,c,cH) -f(t,x1,x2) .* g(t,x1,x2,c{3}) .* cH{
 %% Attention: due to inconsistency the arguments t,x1,x2 appear in different order in swe and transport TODO
 problemData.fCont{1} = @(t,x1,x2) sol_tCont{1}(t,x1,x2) .* problemData.hCont(x1,x2,t) + problemData.solCont{1}(t,x1,x2) .* problemData.h_tCont(x1,x2,t) + ... 
   problemData.u_xCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{1}(t,x1,x2) + problemData.uCont(x1,x2,t) .* problemData.h_xCont(x1,x2,t) .* problemData.solCont{1}(t,x1,x2) + problemData.uCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_xCont{1}(t,x1,x2) + ... 
-  problemData.v_yCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{1}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.h_yCont(x1,x2,t) .* problemData.solCont{1}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_yCont{1}(t,x1,x2) - ...
+  problemData.v_yCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{1}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.h_yCont(x1,x2,t) .* problemData.solCont{1}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_yCont{1}(t,x1,x2) - ... 
   (f(t,x1,x2) .* g(t,x1,x2,problemData.solCont{3}(t,x1,x2)) .* problemData.solCont{1}(t,x1,x2) - h(t,x1,x2,problemData.solCont{1}(t,x1,x2)) .* problemData.solCont{2}(t,x1,x2) - i(t,x1,x2,problemData.solCont{1}(t,x1,x2)) .* problemData.solCont{1}(t,x1,x2)) .* problemData.hCont(x1,x2,t);
 problemData.fCont{2} = @(t,x1,x2) sol_tCont{2}(t,x1,x2) .* problemData.hCont(x1,x2,t) + problemData.solCont{2}(t,x1,x2) .* problemData.h_tCont(x1,x2,t) + ... 
   problemData.u_xCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{2}(t,x1,x2) + problemData.uCont(x1,x2,t) .* problemData.h_xCont(x1,x2,t) .* problemData.solCont{2}(t,x1,x2) + problemData.uCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_xCont{2}(t,x1,x2) + ... 
-  problemData.v_yCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{2}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.h_yCont(x1,x2,t) .* problemData.solCont{2}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_yCont{2}(t,x1,x2) - ...
+  problemData.v_yCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{2}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.h_yCont(x1,x2,t) .* problemData.solCont{2}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_yCont{2}(t,x1,x2) - ... 
   (gamma * h(t,x1,x2,problemData.solCont{1}(t,x1,x2)) .* problemData.solCont{2}(t,x1,x2) - j(t,x1,x2,problemData.solCont{2}(t,x1,x2)) .* problemData.solCont{2}(t,x1,x2)) .* problemData.hCont(x1,x2,t);
 problemData.fCont{3} = @(t,x1,x2) sol_tCont{3}(t,x1,x2) .* problemData.hCont(x1,x2,t) + problemData.solCont{3}(t,x1,x2) .* problemData.h_tCont(x1,x2,t) + ... 
   problemData.u_xCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{3}(t,x1,x2) + problemData.uCont(x1,x2,t) .* problemData.h_xCont(x1,x2,t) .* problemData.solCont{3}(t,x1,x2) + problemData.uCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_xCont{3}(t,x1,x2) + ... 
-  problemData.v_yCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{3}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.h_yCont(x1,x2,t) .* problemData.solCont{3}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_yCont{3}(t,x1,x2) - ...
+  problemData.v_yCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* problemData.solCont{3}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.h_yCont(x1,x2,t) .* problemData.solCont{3}(t,x1,x2) + problemData.vCont(x1,x2,t) .* problemData.hCont(x1,x2,t) .* sol_yCont{3}(t,x1,x2) - ... 
   (-f(t,x1,x2) .* g(t,x1,x2,problemData.solCont{3}(t,x1,x2)) .* problemData.solCont{1}(t,x1,x2) + (1-gamma) .* h(t,x1,x2,problemData.solCont{1}(t,x1,x2)) .* problemData.solCont{2}(t,x1,x2) + i(t,x1,x2,problemData.solCont{1}(t,x1,x2)) .* problemData.solCont{1}(t,x1,x2) + j(t,x1,x2,problemData.solCont{2}(t,x1,x2)) .* problemData.solCont{2}(t,x1,x2)) .* problemData.hCont(x1,x2,t);
 
 % problemData.reactions{1} = @(t,x1,x2,c,cH) 0*x1;
 % problemData.reactions{2} = @(t,x1,x2,c,cH) 0*x1;
 % problemData.reactions{3} = @(t,x1,x2,c,cH) 0*x1;
-
-% problemData.fCont{1} = @(t,x1,x2) 0*x1;
-% problemData.fCont{2} = @(t,x1,x2) 0*x1;
-% problemData.fCont{3} = @(t,x1,x2) 0*x1;
+ 
+% problemData.fCont{1} = @(t,x1,x2) sol_tCont{1}(t,x1,x2) .* problemData.hCont(x1,x2,t) + problemData.solCont{1}(t,x1,x2) .* problemData.h_tCont(x1,x2,t);
+% problemData.fCont{2} = @(t,x1,x2) sol_tCont{2}(t,x1,x2) .* problemData.hCont(x1,x2,t) + problemData.solCont{2}(t,x1,x2) .* problemData.h_tCont(x1,x2,t);
+% problemData.fCont{3} = @(t,x1,x2) sol_tCont{3}(t,x1,x2) .* problemData.hCont(x1,x2,t) + problemData.solCont{3}(t,x1,x2) .* problemData.h_tCont(x1,x2,t);
 
 end % function
