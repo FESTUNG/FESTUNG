66b44b38d2933d6467153baf39ce0001516549f6
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-08-12 19:11:14 +0200

    Rescaled and made minor changes to convergence case. Output is different in some ways. Only numerical change is computation of massFluxQ0T in swe which has to take into account domain boundary conditions for fluxes on boundary. So far this is only done for river boundaries with Riemann solver usage. Convergence of shallow water solution seems to work, convergence of concentrations does not, yet. Ignore changes in advection.

--- postprocessProblem.m
+++ postprocessProblem.m
@@ -1,21 +1,16 @@
 function problemData = postprocessProblem(problemData)
-problemData.errors = zeros(problemData.numSpecies,1);
-for species = 1:problemData.numSpecies
-%   %% Visualization
-%   if problemData.isVisSol{species}
-%     cLagrange = projectDataDisc2DataLagr(problemData.cDisc{species});
-%     visualizeDataLagr(problemData.g, cLagrange, ['cH_' num2str(species) '_h'], problemData.outputBasename{species}, ...
-%                       problemData.numSteps, problemData.outputTypes{species});
-%   end % if
-  %% Error evaluation
-%   fprintf('L2 error w.r.t. the initial condition: %g\n', ...
-  problemData.errors(species) = computeL2Error(problemData.g, problemData.cDisc{species}, @(x1,x2) problemData.solCont{species}(problemData.tEnd,x1,x2), 2*problemData.p, problemData.basesOnQuad);%);
-%   fprintf('norm(cDisc, 1) = %g\n', norm(problemData.cDisc{species}(:), 1));
-%   fprintf('norm(cDisc, 2) = %g\n', norm(problemData.cDisc{species}(:), 2));
-%   fprintf('norm(cDisc, inf) = %g\n', norm(problemData.cDisc{species}(:), inf));
-end % for
-if ~isempty(dataLagr)
-  visualizeDataLagr(problemData.g, dataLagr, varName, problemData.outputBasename, problemData.numSteps, problemData.outputTypes)
+problemData.errors = zeros(problemData.numSpecies,2);
+
+hQ0T = (problemData.hDisc * problemData.basesOnQuad.phi2D{max(2*problemData.p,1)}.');
+
+if problemData.isSolutionAvailable
+  for species = 1:problemData.numSpecies
+    %% Error evaluation
+    problemData.errors(species,1) = computeL2Error(problemData.g, problemData.cDisc{species}, @(x1,x2) problemData.solCont{species}(problemData.tEnd,x1,x2) .* problemData.hCont(x1,x2,problemData.tEnd), 2*problemData.p, problemData.basesOnQuad);
+    dataQ0T = (problemData.cDisc{species} * problemData.basesOnQuad.phi2D{max(2*problemData.p,1)}.') ./ hQ0T;
+    dataDisc = problemData.swe_projectDataQ0T2DataDisc(dataQ0T, 2*problemData.p, problemData.hatM, problemData.basesOnQuad);
+    problemData.errors(species,2) = computeL2Error(problemData.g, dataDisc, @(x1,x2) problemData.solCont{species}(problemData.tEnd,x1,x2), 2*problemData.p, problemData.basesOnQuad);
+  end % for
 end % if
 end % function
 
