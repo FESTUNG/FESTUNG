22f8412eb0b93d898ecdd1e04344a1670436b0c6
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-08-23 18:51:52 +0200

    Interchaanged variable names pd with problemData to match documentation.

--- swe/solveSubStep.m
+++ swe/solveSubStep.m
@@ -55,31 +55,31 @@
 %> along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %> @endparblock
 %
-function pd = solveSubStep(pd, ~, nSubStep)
-K = pd.K;
-N = pd.N;
-dt = pd.dt;
+function problemData = solveSubStep(problemData, ~, nSubStep)
+K = problemData.K;
+N = problemData.N;
+dt = problemData.dt;
 
 % Compute height from potentially different approximation orders:
 % zbDiscLin is always linear (N=3) while cDisc(:,:,1) can be of any
 % approximation order
-hDisc = reshape(execin('swe/computeSumDataDiscDataDisc', pd.cDisc(:,:,1), -pd.zbDiscLin).', [], 1);
+hDisc = reshape(execin('swe/computeSumDataDiscDataDisc', problemData.cDisc(:,:,1), -problemData.zbDiscLin).', [], 1);
 
 % Build right hand side vector
-sysV = cell2mat(pd.globL) - cell2mat(pd.globLRI) - ...
-       [ sparse(K*N,1); pd.nonlinearTerms + pd.bottomFrictionTerms] - ... 
-       pd.riemannTerms;
+sysV = cell2mat(problemData.globL) - cell2mat(problemData.globLRI) - ...
+       [ sparse(K*N,1); problemData.nonlinearTerms + problemData.bottomFrictionTerms] - ... 
+       problemData.riemannTerms;
 
 % Compute solution at next time step using explicit or semi-implicit scheme
-switch pd.schemeType
+switch problemData.schemeType
   case 'explicit'
-    sysA = [ sparse(K*N,K*max(N,3)); pd.tidalTerms{1}; pd.tidalTerms{2} ];
-    cDiscDot = pd.sysW \ (sysV - pd.linearTerms * pd.cDiscRK + sysA * hDisc );
-    pd.cDiscRK = pd.omega(nSubStep) * pd.cDiscRK0 + (1 - pd.omega(nSubStep)) * (pd.cDiscRK + dt * cDiscDot);
+    sysA = [ sparse(K*N,K*max(N,3)); problemData.tidalTerms{1}; problemData.tidalTerms{2} ];
+    cDiscDot = problemData.sysW \ (sysV - problemData.linearTerms * problemData.cDiscRK + sysA * hDisc );
+    problemData.cDiscRK = problemData.omega(nSubStep) * problemData.cDiscRK0 + (1 - problemData.omega(nSubStep)) * (problemData.cDiscRK + dt * cDiscDot);
 
   case 'semi-implicit'
-    sysA = [ sparse(K*N,3*K*N); pd.tidalTerms{1}, sparse(K*N,2*K*N); pd.tidalTerms{2}, sparse(K*N,2*K*N) ];
-    pd.cDiscRK = (pd.sysW + dt * (pd.linearTerms - sysA)) \ (pd.sysW * pd.cDiscRK + dt * sysV);
+    sysA = [ sparse(K*N,3*K*N); problemData.tidalTerms{1}, sparse(K*N,2*K*N); problemData.tidalTerms{2}, sparse(K*N,2*K*N) ];
+    problemData.cDiscRK = (problemData.sysW + dt * (problemData.linearTerms - sysA)) \ (problemData.sysW * problemData.cDiscRK + dt * sysV);
           
   otherwise
     error('Invalid time-stepping scheme')
