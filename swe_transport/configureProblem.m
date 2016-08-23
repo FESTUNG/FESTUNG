b5db8e4258ad2a44aac76bcb5d1da1dbca7eeea0
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-08-23 18:56:34 +0200

    Added documentation for swe_transport and transport (copied from routines in swe). Everything in swe, swe_transport and transport is documented, there might be bugs/typos of course.

--- swe_transport/configureProblem.m
+++ swe_transport/configureProblem.m
@@ -1,3 +1,51 @@
+% Fills the problemData-struct with all basic configuration options.
+% Problem parameters are to be modified inside this routine.
+
+%===============================================================================
+%> @file template/configureProblem.m
+%>
+%> @brief Fills the problemData-struct with all basic configuration options.
+%>        Problem parameters are to be modified inside this routine.
+%===============================================================================
+%>
+%> @brief Fills the problemData-struct with all basic configuration options.
+%>        Problem parameters are to be modified inside this routine.
+%>
+%> This routine is called before any other function for the problem.
+%> It should define all problem parameters.
+%>
+%> The struct must provide a positive numeric parameter <code>numSteps</code>
+%> that specifies the number of iterative steps (e.g., number of time
+%> steps) which are to be executed in the main part of the solution
+%> algorithm.
+%> For stationary problems set this to 1.
+%>
+%> @param  problemData  A (probably) empty struct with problem parameters.
+%>                      @f$[\text{struct}]@f$
+%>
+%> @retval problemData  A struct with all necessary parameters and definitions
+%>                      for the problem description. @f$[\text{struct}]@f$
+%>
+%> This file is part of FESTUNG
+%>
+%> @copyright 2014-2016 Hennes Hajduk, Balthasar Reuter, Florian Frank, Vadym Aizinger
+%> 
+%> @par License
+%> @parblock
+%> This program is free software: you can redistribute it and/or modify
+%> it under the terms of the GNU General Public License as published by
+%> the Free Software Foundation, either version 3 of the License, or
+%> (at your option) any later version.
+%>
+%> This program is distributed in the hope that it will be useful,
+%> but WITHOUT ANY WARRANTY; without even the implied warranty of
+%> MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%> GNU General Public License for more details.
+%>
+%> You should have received a copy of the GNU General Public License
+%> along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%> @endparblock
+%
 function problemData = configureProblem(problemData)
 
 %% Configuration to use: 
--- transport/solveSubStep.m
+++ transport/solveSubStep.m
@@ -1,3 +1,60 @@
+% Second step of the three-part algorithm in the iterateSubSteps loop of each
+% step in the main loop.
+
+%===============================================================================
+%> @file template/solveSubStep.m
+%>
+%> @brief Second step of the three-part algorithm in the iterateSubSteps loop of
+%>        each step in the main loop.
+%===============================================================================
+%>
+%> @brief Second step of the three-part algorithm in the iterateSubSteps loop of
+%>        each step in the main loop.
+%>
+%> The iterateSubSteps loop repeatedly executes three steps until the number of
+%> substep iterations equals the order of the underlying Runge-Kutta method.
+%> These three steps are:
+%>
+%>  1. preprocessSubStep()
+%>  2. solveSubStep()
+%>  3. postprocessSubStep()
+%> 
+%> This routine is executed second in each substep loop iteration and is
+%> intended to produce the solution at the next substep, e.g., at the time-level
+%> of the new Runge-Kutta step.
+%>
+%> @param  problemData  A struct with problem parameters, precomputed
+%>                      fields, and solution data structures (either filled
+%>                      with initial data or the solution from the previous
+%>                      loop iteration), as provided by configureProblem()  
+%>                      and preprocessProblem(). @f$[\text{struct}]@f$
+%> @param  nSubStep     The current iteration number of the iterateSubSteps 
+%>                      loop. 
+%>
+%> @retval problemData  The input struct enriched with solution data at
+%>                      the next substep. @f$[\text{struct}]@f$
+%>
+%> This file is part of FESTUNG
+%>
+%> @copyright 2014-2016 Hennes Hajduk, Balthasar Reuter, Florian Frank, Vadym Aizinger
+%>
+%> 
+%> @par License
+%> @parblock
+%> This program is free software: you can redistribute it and/or modify
+%> it under the terms of the GNU General Public License as published by
+%> the Free Software Foundation, either version 3 of the License, or
+%> (at your option) any later version.
+%>
+%> This program is distributed in the hope that it will be useful,
+%> but WITHOUT ANY WARRANTY; without even the implied warranty of
+%> MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%> GNU General Public License for more details.
+%>
+%> You should have received a copy of the GNU General Public License
+%> along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%> @endparblock
+%
 function problemData = solveSubStep(problemData, ~, nSubStep)
 K = problemData.K;
 N = problemData.N;
