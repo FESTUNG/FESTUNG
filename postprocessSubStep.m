8b8737f494bb2f12486e4903d87a1a317f13a78e
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-08-23 18:51:52 +0200

    Interchaanged variable names pd with problemData to match documentation.

--- postprocessSubStep.m
+++ postprocessSubStep.m
@@ -37,7 +37,7 @@
 %> This file is part of FESTUNG
 %>
 %> @copyright 2014-2016 Balthasar Reuter, Florian Frank, Vadym Aizinger
-%>                      Modified 08/17/16 by Hennes Hajduk
+%>                      Modified 08/23/16 by Hennes Hajduk
 %>
 %> @par License
 %> @parblock
@@ -55,20 +55,20 @@
 %> along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %> @endparblock
 %
-function pd = postprocessSubStep(pd, nStep, nSubStep)
-K = pd.K;
-N = pd.N;
+function problemData = postprocessSubStep(problemData, nStep, nSubStep)
+K = problemData.K;
+N = problemData.N;
 
 % Reshape linearized vector to solution vectors
-pd.cDisc(:,:,1) = reshape(pd.cDiscRK(        1 :   K*N), N, K).';
-pd.cDisc(:,:,2) = reshape(pd.cDiscRK(  K*N + 1 : 2*K*N), N, K).';
-pd.cDisc(:,:,3) = reshape(pd.cDiscRK(2*K*N + 1 : 3*K*N), N, K).';
+problemData.cDisc(:,:,1) = reshape(problemData.cDiscRK(        1 :   K*N), N, K).';
+problemData.cDisc(:,:,2) = reshape(problemData.cDiscRK(  K*N + 1 : 2*K*N), N, K).';
+problemData.cDisc(:,:,3) = reshape(problemData.cDiscRK(2*K*N + 1 : 3*K*N), N, K).';
 
 % TODO: slope limiting here
 
 % Ensure water height doesn't fall below threshold
-pd.cDisc(:,:,1) = pd.swe_correctMinValueExceedanceDisc(pd.cDisc(:,:,1), pd.sysMinValueCorrection, nStep, pd.zbLagr + pd.minTol, pd.elevTol);
-pd.cDiscRK(1 : pd.K*pd.N) = reshape(pd.cDisc(:,:,1).', pd.N * pd.K, 1);
+problemData.cDisc(:,:,1) = problemData.swe_correctMinValueExceedanceDisc(problemData.cDisc(:,:,1), problemData.sysMinValueCorrection, nStep, problemData.zbLagr + problemData.minTol, problemData.elevTol);
+problemData.cDiscRK(1 : problemData.K*problemData.N) = reshape(problemData.cDisc(:,:,1).', problemData.N * problemData.K, 1);
 
-pd.isSubSteppingFinished = nSubStep >= length(pd.tLvls);
+problemData.isSubSteppingFinished = nSubStep >= length(problemData.tLvls);
 end % function
