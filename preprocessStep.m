c8d22dc80980b62e2c3bf3639e46be000cc5e937
Balthasar Reuter <balthasar.reuter@fau.de>
2016-09-14 12:40:37 +0200

    Remaining functions for darcy problem added. Convergence for linear functions checked

--- preprocessStep.m
+++ preprocessStep.m
@@ -4,30 +4,31 @@ t = nStep * problemData.tau;
 
 %% L2-projections of algebraic coefficients.
 KDisc = cellfun(@(c) projectFuncCont2DataDiscTrap(problemData.g, @(x1,x2) c(t,x1,x2), problemData.qOrd, ...
-                                                  problemData.hatM{1}, problemData.basesOnQuad), ...
-                problemData.KCont, 'UniformOutput', false);
+        problemData.hatM{1}, problemData.basesOnQuad), problemData.KCont, 'UniformOutput', false);
 fDisc = projectFuncCont2DataDiscTrap(problemData.g, @(x1,x2) problemData.fCont(t,x1,x2), problemData.qOrd, ...
-                                     problemData.hatM{1}, problemData.basesOnQuad);
+        problemData.hatM{1}, problemData.basesOnQuad);
                              
 %% Assembly of time-dependent global matrices.
 problemData.globG = assembleMatElemTrapDphiPhiFuncDisc(problemData.g, problemData.hatG, KDisc);
 problemData.globR = assembleMatEdgeTrapPhiPhiFuncDiscNu(problemData.g, problemData.g.markE0Tint, ...
-                                                        problemData.hatRdiag, problemData.hatRoffdiag, KDisc);
+                    problemData.hatRdiag, problemData.hatRoffdiag, KDisc);
 
 %% Assembly of Dirichlet boundary contributions.
-cDCont = @(x1,x2) problemData.cDCont(t,x1,x2);
-problemData.globRD = assembleMatEdgeTrapPhiIntPhiIntFuncDiscIntNu(problemData.g, problemData.g.markE0TbdrD, ...
-                                                                  problemData.hatRdiag, KDisc);
-problemData.globJD = assembleVecEdgePhiIntFuncContNu(problemData.g, problemData.g.markE0TbdrD, ...
-                      cDCont, problemData.N, problemData.basesOnQuad, problemData.g.areaNuE0TbdrD);
-problemData.globKD = problemData.eta * assembleVecEdgePhiIntFuncCont(problemData.g, problemData.g.markE0TbdrD, ...
-                      cDCont, problemData.N, problemData.basesOnQuad);
+hDCont = @(x1,x2) problemData.hDCont(t,x1,x2);
+problemData.globRD = assembleMatEdgeTrapPhiIntPhiIntFuncDiscIntNu(problemData.g, ...
+                     problemData.g.markE0TbdrD, problemData.hatRdiag, KDisc);
+problemData.globJD = assembleVecEdgeTrapPhiIntFuncContNu(problemData.g, problemData.g.markE0TbdrD, ...
+                     hDCont, problemData.N, problemData.qOrd, problemData.basesOnQuad);
+problemData.globKD = problemData.eta * assembleVecEdgeTrapPhiIntFuncCont(problemData.g, ...
+                     problemData.g.markE0TbdrD, hDCont, problemData.N, problemData.qOrd, problemData.basesOnQuad);
                   
 %% Assembly of Neumann boundary contributions.
-problemData.globKN = assembleVecEdgePhiIntFuncDiscIntFuncCont(problemData.g, problemData.g.markE0TbdrN, ...
-                      dDisc, @(x1,x2) problemData.gNCont(t,x1,x2), problemData.basesOnQuad, problemData.g.areaE0TbdrN);
-                  
+gNCont = @(x1,x2) problemData.gNCont(t,x1,x2);
+problemData.globKN = assembleVecEdgeTrapPhiIntFuncCont(problemData.g, ...
+                     problemData.g.markE0TbdrN .* problemData.g.areaE0T, ...
+                     gNCont, problemData.N, problemData.qOrd, problemData.basesOnQuad);
+                   
 %% Assembly of the source contribution.
-problemData.globL = problemData.globM * reshape(fDisc', problemData.K * problemData.N, 1);
+problemData.globL = problemData.globM * reshape(fDisc', problemData.g.numT * problemData.N, 1);
 
 end % function
