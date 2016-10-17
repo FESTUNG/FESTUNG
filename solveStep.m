c512fffaf593867b1c84b1079312639fb6778f24
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-10-17 16:44:34 +0200

    Added ADCIRC as configSource in transport. Defined logical isMask per species, added distinction between vertex-based and element-based masks. Changed structure such that slope limiting is applied to c instead of cH. Set boundary conditions for transport problem to make sense for all types of swe-meshes. (land = Neumann, rest = Dirichlet, this makes also sense for radiation bdrs.) Assembly of stiffness matrix for each species individually.

--- solveStep.m
+++ solveStep.m
@@ -52,13 +52,12 @@
 %> @endparblock
 %
 function problemData = solveStep(problemData, nStep)
-KN = problemData.K * problemData.N;
 
 % Obtain Runge-Kutta rule
 [problemData.timeLvls, problemData.omega] = rungeKuttaSSP(problemData.ordRK, problemData.tau, (nStep - 1) * problemData.tau);
 
 % Linearize solution vector
-problemData.cDiscRK0 = cellfun(@(c) reshape(c.', [KN,1]), problemData.cDisc, 'UniformOutput', false);
+problemData.cDiscRK0 = cellfun(@(c) reshape(c.', [problemData.K * problemData.N, 1]), problemData.cDisc, 'UniformOutput', false);
 problemData.cDiscRK = problemData.cDiscRK0;
 
 % Initialize substepping
