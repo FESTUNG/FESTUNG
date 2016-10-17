95d7315165e75bc9cc1eb9747d2f29bdd8264c53
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-10-17 16:49:16 +0200

    Fixed bug: Removed factor 0.5 from assembly routine.

--- assembleMatEdgePhiIntPerQuad.m
+++ assembleMatEdgePhiIntPerQuad.m
@@ -103,9 +103,9 @@ validateattributes(refEdgePhiIntPerQuad, {'numeric'}, {'size', [NaN NaN 3]}, mfi
 ret = cell(3,1);
 for n = 1 : 3
   if nargin > 3
-    ret{n} = 0.5 * kron( spdiags(areaE0Tbdr{n}, 0, K, K), refEdgePhiIntPerQuad(:,:,n) );
+    ret{n} = kron( spdiags(areaE0Tbdr{n}, 0, K, K), refEdgePhiIntPerQuad(:,:,n) );
   else
-    ret{n} = 0.5 * kron( spdiags(markE0Tbdr(:,n) .* g.areaE0T(:,n), 0, K, K), refEdgePhiIntPerQuad(:,:,n) );
+    ret{n} = kron( spdiags(markE0Tbdr(:,n) .* g.areaE0T(:,n), 0, K, K), refEdgePhiIntPerQuad(:,:,n) );
   end % if
 end % for
 end % function
