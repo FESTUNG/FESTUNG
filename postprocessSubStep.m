c9613129a07270f04f7392f2e56486e6c1d5388b
Hennes Hajduk <hennes_hajduk@yahoo.de>
2016-12-01 12:12:58 +0100

    Cleaned (swe-)transport code up.

--- postprocessSubStep.m
+++ postprocessSubStep.m
@@ -57,6 +57,5 @@
 function problemData = postprocessSubStep(problemData, nStep, nSubStep)
 problemData.sweData = problemData.swe_postprocessSubStep(problemData.sweData, nStep, nSubStep);
 problemData.transportData = problemData.transport_postprocessSubStep(problemData.transportData, nStep, nSubStep);
-
-problemData.isSubSteppingFinished = problemData.transportData.isSubSteppingFinished; % TODO change if different RK schemes should be allowed
+problemData.isSubSteppingFinished = problemData.transportData.isSubSteppingFinished;
 end % function
