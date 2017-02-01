40562fca168d12311ea7ec5243a138a3299e6553
Balthasar Reuter <balthasar.reuter@fau.de>
2017-02-01 15:22:39 +0100

    Moved generating coord-dependend grid data (e.g. area, bary, nu, ...) to a separate function as preparation for mesh adaptivity

--- darcyVert/domainRectTrap.m
+++ darcyVert/domainRectTrap.m
@@ -57,16 +57,6 @@ g.V0E(1:g.numT, :) = g.V0T(:, 1:2); % lower edges in all elements
 g.V0E(g.numT:g.numT + g.numElem(1),:) = g.V0T((g.numElem(2) - 1) *  g.numElem(1) : end, [4 3]); % upper edges at top boundary
 g.V0E(g.numElem(1) * (g.numElem(2) + 1) + 1 : g.numE - g.numElem(2), :) = g.V0T(:,[1 4]); % left edges
 g.V0E(g.numE - g.numElem(2) + 1 : end, :) = g.V0T(g.numElem(1) : g.numElem(1) : end, [2 3]); % right edges
-%% Edge lengths and normals (areaE, areaE0T, nuE)
-vecE = g.coordV(g.V0E(:, 2), :) - g.coordV(g.V0E(:, 1), :);
-areaE = sqrt(vecE(:, 1).^2 + vecE(:, 2).^2);
-nuE = vecE * [0, -1; 1, 0] ./ areaE(:, [1, 1]);
-g.areaE0T = areaE(g.E0T);
-g.nuE0T = zeros(g.numT, 4, 2);
-g.nuE0T(:,1,:) = nuE(g.E0T(:,1), :);
-g.nuE0T(:,2,:) = -nuE(g.E0T(:,2), :);
-g.nuE0T(:,3,:) = nuE(g.E0T(:,3), :);
-g.nuE0T(:,4,:) = -nuE(g.E0T(:,4), :);
 %% Mapping of neighbouring edges (markE0TE0T)
 g.markE0TE0T = cell(1,4);
 mapE0E = [2 1 4 3];
@@ -85,31 +75,8 @@ g.coordV0T = zeros(g.numT, 4, 2);
 for k = 1 : 4
   g.coordV0T(:, k, :) = g.coordV(g.V0T(:, k), :);
 end % for
-%% Element centroids (baryT)
-g.baryT = squeeze(sum(g.coordV0T, 2)) / 4;
-%% Edge centroids (baryE, baryE0T)
-g.baryE = 0.5 * (g.coordV(g.V0E(:,1),:) + g.coordV(g.V0E(:, 2),:));
-g.baryE0T = zeros(g.numT, 4, 2);
-for k = 1 : 4
-  g.baryE0T(:, k, :) = squeeze(g.baryE(g.E0T(:,k), :));
-end % for
-%% Jacobian of the mapping (J0T) and its determinant (detJ0T), 
-% split into constant (1), x1- (2) and x2-contributions (3)
-g.J0T = { zeros(g.numT, 2, 2), zeros(g.numT, 2, 2), zeros(g.numT, 2, 2) };
-g.J0T{1}(:,1,1) = g.coordV0T(:,2,1) - g.coordV0T(:,1,1);
-g.J0T{1}(:,2,1) = g.coordV0T(:,2,2) - g.coordV0T(:,1,2);
-g.J0T{1}(:,2,2) = g.coordV0T(:,4,2) - g.coordV0T(:,1,2);
-g.J0T{2}(:,2,2) = (g.coordV0T(:,3,2) - g.coordV0T(:,2,2)) - (g.coordV0T(:,4,2) - g.coordV0T(:,1,2));
-g.J0T{3}(:,2,1) = (g.coordV0T(:,3,2) - g.coordV0T(:,2,2)) - (g.coordV0T(:,4,2) - g.coordV0T(:,1,2));
-g.detJ0T = cell(1,3);
-for s = 1 : 3
-  g.detJ0T{s} = g.J0T{s}(:,1,1) .* g.J0T{s}(:,2,2) - g.J0T{s}(:,2,1) .* g.J0T{s}(:,1,2);
-end % for
-%% Element area (areaT)
-g.areaT = 0.5 * (g.areaE0T(:,3) + g.areaE0T(:,4)) .* g.J0T{1}(:,1,1);
-%% Mapping from reference element to physical element (mapRef2Phy)
-g.mapRef2Phy = @(i,X1,X2) g.J0T{1}(:,i,1) * X1 + g.J0T{1}(:,i,2) * X2 + ...
-                          g.J0T{2}(:,i,2) * (X1 .* X2) + g.coordV0T(:,1,i) * ones(size(X1));
+%% Generate grid data that changes when coordV, coordV0T changes
+g = generateCoordDependGridData(g);
 %% 1D mesh for surface
 g.g1D.numT = g.numElem(1);
 g.g1D.numV = g.g1D.numT + 1;
