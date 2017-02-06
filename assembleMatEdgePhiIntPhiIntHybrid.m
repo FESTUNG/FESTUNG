9fc2a15b958058ca1ad224faf7b7bdb4d8d510b6
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-02-06 18:13:57 +0100

    function to integrate phi*phi on edge w/o respecting boundary conditions

---
+++ assembleMatEdgePhiIntPhiIntHybrid.m
@@ -0,0 +1,25 @@
+%
+function ret = assembleMatEdgePhiIntPhiIntHybrid(g, refEdgePhiIntPhiInt)
+% Extract dimensions
+K = g.numT;  N = size(refEdgePhiIntPhiInt, 1);
+
+% Check function arguments that are directly used
+validateattributes(refEdgePhiIntPhiInt, {'numeric'}, {'size', [N N 3]}, mfilename, 'refEdgePhiIntPhiInt');
+
+% Assemble matrix
+% ret = sparse(K*N, K*N);
+% for n = 1 : 3
+%   ret = ret + kron(spdiags(g.areaE,0,K,K), refEdgePhiIntPhiInt(:,:,n));
+% end % for
+
+for iT = 1:K
+    for iE = 1:3
+        iTs = (iT-1)*N + 1;
+        iTe = (iT)*N;
+        iEs = (g.E0T(iT, iE) - 1)*Nlambda + 1;
+        iEe = (g.E0T(iT, iE))*Nlambda;
+        ret( iTs:iTe,  iTs:iTe  ) = ret( iTs:iTe,  iTs:iTe ) + g.areaE( g.E0T(iT, iE) ) .* refEdgePhiIntPhiInt;
+%         ret = ret + kron(spdiags(g.areaE,0,K,K), refEdgePhiIntPhiInt(:,:,n));
+    end
+end
+end % function
