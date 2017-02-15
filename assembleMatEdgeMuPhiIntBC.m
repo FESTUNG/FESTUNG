38b9ebcd013b7f05ef785ec5881552259fd37496
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-02-15 10:52:42 +0100

    function for edge integrals; i don't use it at the moment

---
+++ assembleMatEdgeMuPhiIntBC.m
@@ -0,0 +1,29 @@
+%
+%
+function ret = assembleMatEdgeMuPhiIntBC(g, markInt, refEdgePhiIntMu)
+% Extract dimensions
+K = g.numT;  N = size(refEdgePhiIntMu, 1);
+KEdge = g.numE; Nlambda = size(refEdgePhiIntMu, 2);
+
+% Check function arguments that are directly used
+% validateattributes(markE0Tbdr, {'logical'}, {'size', [K 3]}, mfilename, 'markE0Tbdr');
+validateattributes(refEdgePhiIntMu, {'numeric'}, {'size', [N Nlambda 3 2]}, mfilename, 'refEdgePhiIntMu');
+
+% Assemble matrix
+ret = sparse(K*N, KEdge*Nlambda);
+for iT = 1:K
+    for iE = 1:3
+        edgeNr = g.E0T(iT, iE);
+        flip = 1;        
+        if (g.T0E(edgeNr, 2) == iT)
+           flip = 2; 
+        end
+        iTs = (iT-1)*N + 1;
+        iTe = (iT)*N;
+        iEs = (edgeNr - 1)*Nlambda + 1;
+        iEe = (edgeNr)*Nlambda;
+        ret( iTs:iTe,  iEs:iEe  ) = ret( iTs:iTe, iEs:iEe ) + markInt( iT, iE ) .* g.areaE0T( iT, iE ) .* refEdgePhiIntMu(:,:, iE, flip);
+    end
+end
+
+end % function
