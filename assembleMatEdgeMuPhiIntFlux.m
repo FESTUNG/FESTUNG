5cd1179d147c6a2f90e3cd2842ccb78fc3be4cac
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-02-16 16:23:08 +0100

    optimized version of assembleMatEdgeMuPhiIntFlux. It is about 60 times faster than the old version

--- assembleMatEdgeMuPhiIntFlux.m
+++ assembleMatEdgeMuPhiIntFlux.m
@@ -4,40 +4,22 @@ function ret = assembleMatEdgeMuPhiIntFlux( g, markE0Tbdr, N, Nlambda, uEdge, Sb
 K = g.numT;
 KEdge = g.numE;
 
+[N, Nlambda, ~, R] = size(Sbar{1});
 % Assemble matrix
 ret = cell(2,1);
 ret{1} = sparse( K*N, KEdge*Nlambda);
 ret{2} = sparse( K*N, KEdge*Nlambda);
-
-for iT = 1:K
+for iDim = 1:2
     for iE = 1:3
-        edgeNr = g.E0T(iT, iE);
-        
-        uEdgeLocal = uEdge( :, :, iT, iE);
-        flip = 1;
-        %         if (g.T0E(edgeNr, 2) == iT)
-        if (g.T0E(edgeNr, 2) == iT)
-            %         if (2 == iT)
-            flip = 2;
-%             uEdgeLocal = fliplr( uEdgeLocal );
-        end
-        
-        iTs = (iT-1)*N + 1;
-        iTe = (iT)*N;
-        iEs = (g.E0T(iT, iE) - 1)*Nlambda + 1;
-        iEe = (g.E0T(iT, iE))*Nlambda;
-        
-        for iDim = 1:2
-            tmp = zeros(N, Nlambda);
-            for i = 1:N
-                for j=1:Nlambda
-                    %                     tmp(i,j) = uEdge( iDim, :, iT, iE) * Sbar( :, i, j, iDim, flip);
-                    tmp(i,j) = uEdgeLocal( iDim, :) .* g.nuE0T( iT, iE , iDim) * Sbar( :, i, j, iE, flip);
-                end
-            end
-            
-            ret{iDim}( iTs:iTe,  iEs:iEe ) = ret{iDim}( iTs:iTe, iEs:iEe ) ...
-                + markE0Tbdr( iT, iE ) .* g.areaE( edgeNr ) .* tmp;
+        Rkn1 = markE0Tbdr(:,iE) .* g.areaE0T(:, iE) .*  g.flipArray(:, iE) .* g.nuE0T( :, iE , iDim);
+        Rkn2 = markE0Tbdr(:,iE) .* g.areaE0T(:, iE) .* ~g.flipArray(:, iE) .* g.nuE0T( :, iE , iDim);
+        for r = 1:R
+            ret{iDim} = ret{iDim} + ...
+                kron( sparse( 1:K, g.E0T(:, iE), ones(K, 1) .* uEdge{iDim}( r, :, iE)' .* Rkn1 , K, KEdge ), ...
+                Sbar{1}( :, :, iE, r) );
+            ret{iDim} = ret{iDim} + ...
+                kron( sparse( 1:K, g.E0T(:, iE), ones(K, 1) .* uEdge{iDim}( r, :, iE)' .* Rkn2 , K, KEdge ), ...
+                Sbar{2}( :, :, iE, r) );
         end
     end
 end
