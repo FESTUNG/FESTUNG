719eb9a7a8cca145a0818849184a36044bee9ee9
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-02-16 16:54:54 +0100

    opimized version of assembleMatEdgePhiIntMu.m

--- assembleMatEdgePhiIntMu.m
+++ assembleMatEdgePhiIntMu.m
@@ -9,27 +9,13 @@ Kedge = g.numE; Nlambda = size(refEdgePhiIntMu, 1);
 validateattributes(markE0T, {'logical'}, {'size', [K 3]}, mfilename, 'markE0T');
 validateattributes(refEdgePhiIntMu, {'numeric'}, {'size', [Nlambda N 3 2]}, mfilename, 'refEdgePhiIntMu');
 
-% Assemble matrix
 ret = sparse(Kedge*Nlambda, K*N);
-for iT = 1:K
-    for iE = 1:3
-        edgeNr = g.E0T(iT, iE);
-        
-        flip = 1;        
-        if (g.T0E(edgeNr, 2) == iT)
-           flip = 2; 
-        end
-        
-        iTs = (iT-1)*N + 1;
-        iTe = (iT)*N;
-        iEs = (g.E0T(iT, iE) - 1)*Nlambda + 1;
-        iEe = (g.E0T(iT, iE))*Nlambda;
-        ret( iEs:iEe, iTs:iTe ) = ret( iEs:iEe, iTs:iTe ) + markE0T(iT,iE) .* g.areaE( edgeNr ) .* refEdgePhiIntMu(:,:, iE, flip);
-    end
-end
-
-
-% for n = 1 : 3
-%   ret = ret + kron(spdiags(markE0Tint(:,n),0,Kedge,K), refEdgePhiIntMu(:,:));
-% end % for
+for iE = 1:3
+    Rkn1 = markE0T(:,iE) .*  g.flipArray(:, iE) .* g.areaE0T(:, iE);
+    Rkn2 = markE0T(:,iE) .* ~g.flipArray(:, iE) .* g.areaE0T(:, iE);
+    ret = ret + kron(sparse( g.E0T(:, iE), 1:K, Rkn1, Kedge, K ), ...
+              refEdgePhiIntMu(:,:,iE, 1));
+    ret = ret + kron(sparse( g.E0T(:, iE), 1:K, Rkn2, Kedge, K ), ...
+              refEdgePhiIntMu(:,:,iE, 2));
+end % for
 end % function
