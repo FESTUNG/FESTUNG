a126cd4ca996820ea80c56a6b9cfabd6147fcf84
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-02-16 16:16:07 +0100

    moved data into cells

--- evalUContAtEveryEdgeIntPoint.m
+++ evalUContAtEveryEdgeIntPoint.m
@@ -5,7 +5,10 @@ p = Nlambda-1;  qOrd = max(2*p, 1);  [Q1,~] = quadRule1D(qOrd);
 K = g.numT;
 Kedge = g.numE;
 
-dataEval = zeros( 2, size(Q1,2), K, 3 );
+% dataEval = zeros( 2, size(Q1,2), K, 3 );
+dataEval = cell(2,1);
+dataEval{1} = zeros( size(Q1,2), K, 3 );
+dataEval{2} = zeros( size(Q1,2), K, 3 );
 
 
 for iT = 1:K
@@ -18,7 +21,10 @@ for iT = 1:K
         F1 = @(X1, X2) g.B(adjTri,1,1)*X1 + g.B(adjTri,1,2)*X2 + g.coordV0T(adjTri,1,1)*ones(size(X1));
         F2 = @(X1, X2) g.B(adjTri,2,1)*X1 + g.B(adjTri,2,2)*X2 + g.coordV0T(adjTri,1,2)*ones(size(X1));
         
-        dataEval(:, :, iT, iE)  = cCont( F1(x1, x2), F2(x1, x2) );
+%         dataEval(:, :, iT, iE)  = cCont( F1(x1, x2), F2(x1, x2) );
+        res = cCont( F1(x1, x2), F2(x1, x2) );
+        dataEval{1}(:, iT, iE) = res(1, :);
+        dataEval{2}(:, iT, iE) = res(2, :);
 
 %         dataEval(:, :, iT, iE)  = (~g.markE0Tint(iT, iE)) * cCont( F1(x1, x2), F2(x1, x2) );
     end
--- integrateRefEdgeMuPhiIntFlux.m
+++ integrateRefEdgeMuPhiIntFlux.m
@@ -3,14 +3,17 @@ function ret = integrateRefEdgeMuPhiIntFlux(N, Nlambda, basesOnQuad, basesOnGamm
 validateattributes(basesOnQuad, {'struct'}, {}, mfilename, 'basesOnQuad')
 p = (sqrt(8*N+1)-3)/2;  qOrd = 2*p+1;  [~, W] = quadRule1D(qOrd);
 Nip = size(W,2);
-ret = zeros(Nip, N, Nlambda, 3, 2); % [N x N x 3]
+%ret = zeros(Nip, N, Nlambda, 3, 2); % [N x N x 3]
+ret = cell(2,1);
+ret{1} = zeros(N, Nlambda, 3, Nip); % [N x N x 3]
+ret{2} = zeros(N, Nlambda, 3, Nip); % [N x N x 3]
 for n = 1 : 3 % 3 edges
     for i = 1 : N
         for j = 1 : Nlambda
             for ip = 1:Nip
-                ret(ip, i, j, n, 1) = W(ip) .* basesOnQuad.phi1D{qOrd}(ip,i,n) .* basesOnGamma.phi1D{qOrd}(ip,j) ;
+                ret{1}(i, j, n, ip) = W(ip) .* basesOnQuad.phi1D{qOrd}(ip,i,n) .* basesOnGamma.phi1D{qOrd}(ip,j) ;
                 baseFlip = flipud(basesOnGamma.phi1D{qOrd});
-                ret(ip, i, j, n, 2) = W(ip) * (basesOnQuad.phi1D{qOrd}(ip,i,n) .* baseFlip(ip,j) );
+                ret{2}(i, j, n, ip) = W(ip) * (basesOnQuad.phi1D{qOrd}(ip,i,n) .* baseFlip(ip,j) );
             end
         end % for
     end % for
