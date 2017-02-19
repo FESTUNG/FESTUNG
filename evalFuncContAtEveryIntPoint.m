4ee64f04beef2fbe09e682d4c33eb7c0ebac06c8
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-02-19 11:13:07 +0100

    more cleanup

--- evalFuncContAtEveryIntPoint.m
+++ evalFuncContAtEveryIntPoint.m
@@ -1,11 +1,12 @@
 function dataEval = evalFuncContAtEveryIntPoint(g, funcCont, N, basesOnQuad)
 validateattributes(funcCont, {'function_handle'}, {}, mfilename, 'funcCont');
 validateattributes(basesOnQuad, {'struct'}, {}, mfilename, 'basesOnQuad');
-
+K = g.numT;
 p = (sqrt(8*N+1)-3)/2;  qOrd = max(2*p, 1);  [Q1, Q2, ~] = quadRule2D(qOrd);
 
 F1 = @(X1, X2) g.B(:,1,1)*X1 + g.B(:,1,2)*X2 + g.coordV0T(:,1,1)*ones(size(X1));
 F2 = @(X1, X2) g.B(:,2,1)*X1 + g.B(:,2,2)*X2 + g.coordV0T(:,1,2)*ones(size(X1));
 
-dataEval = funcCont(F1(Q1, Q2), F2(Q1, Q2));
+dataEval = zeros( K, size(Q1, 2), 2 );
+[dataEval(:,:,1), dataEval(:,:,2)] = funcCont(F1(Q1, Q2), F2(Q1, Q2));
 end % function
--- hdg_advection/configureProblem.m
+++ hdg_advection/configureProblem.m
@@ -61,11 +61,11 @@
 function problemData = configureProblem(problemData)
 %% Parameters.
 %problemData.hmax        = 2^-3; % maximum edge length of triangle
-problemData.hmax        = 2^-4; % maximum edge length of triangle
+problemData.hmax        = 2^-2; % maximum edge length of triangle
 problemData.p           = 3; % local polynomial degree
 problemData.ordRK       = 4; % order of Runge Kutta time stepper.
 % problemData.ordRK       = min(problemData.p+1,4); % order of Runge Kutta time stepper.
-problemData.numSteps    = 16; % number of time steps
+problemData.numSteps    = 8; % number of time steps
 problemData.tEnd        = pi/(4); % end time
 
 problemData.isVisGrid   = false; % visualization of grid
