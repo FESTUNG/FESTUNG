6cc2340548f2cfbf89d1818e22f775f35175a689
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-02-24 09:19:40 +0100

    function to determin block size of local solves

---
+++ determineLocalSolveBlockSize.m
@@ -0,0 +1,10 @@
+function localSolveBlockSize = determineLocalSolveBlockSize( problemData )
+    K = problemData.K;
+    localSolveBlockSize = min(K, 16);
+    if (mod(K, localSolveBlockSize) ~= 0)
+        localSolveBlockSize = 1;
+        warning('Block size does not fit the problem. Reset block size to 1');
+    end
+    assert( mod(K, localSolveBlockSize) == 0, ...
+        'Block size does not fit to problem size!');
+end
