e23495b08fd1b684b28fcbee90539435d4241593
Balthasar Reuter <balthasar.reuter@fau.de>
2017-03-09 17:02:25 +0100

    Performance optimization

--- assembleMatEdgeTrapPhiPhiNu.m
+++ assembleMatEdgeTrapPhiPhiNu.m
@@ -1,12 +1,15 @@
-function ret = assembleMatEdgeTrapPhiPhiNu(g, markE0Tint, refEdgePhiIntPhiInt, refEdgePhiIntPhiExt, edgeIndex)
+function ret = assembleMatEdgeTrapPhiPhiNu(g, markE0Tint, refEdgePhiIntPhiInt, refEdgePhiIntPhiExt, edgeIndex, normalIndex)
 if nargin < 5
   edgeIndex = 1 : 4;
 end % if
+if nargin < 6
+  normalIndex = 1 : 2;
+end % if
 K = g.numT; N = size(refEdgePhiIntPhiInt, 1);
-ret = { sparse(K*N, K*N), sparse(K*N, K*N) };
+ret = cellfun(@(c) sparse(K*N, K*N), cell(size(normalIndex)), 'UniformOutput', false); %{ sparse(K*N, K*N), sparse(K*N, K*N) };
 for n = edgeIndex
   areaE0Tint = 0.5 * markE0Tint(:,n) .* g.areaE0T(:,n);
-  for m = 1 : 2
+  for m = normalIndex
     areaNuE0Tint = areaE0Tint .* g.nuE0T(:,n,m);
     ret{m} = ret{m} + ...
              kron(spdiags(areaNuE0Tint, 0, K, K ) , refEdgePhiIntPhiInt(:,:,n)) + ...
