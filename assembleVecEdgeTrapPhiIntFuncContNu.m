aabe3d1158e94c795e96c6a96114446fab18681e
Balthasar Reuter <balthasar.reuter@fau.de>
2017-03-16 17:55:34 +0100

    Fixed remaining errors. Testcases up to quadratic are solved correctly

--- assembleVecEdgeTrapPhiIntFuncContNu.m
+++ assembleVecEdgeTrapPhiIntFuncContNu.m
@@ -1,4 +1,30 @@
 function ret = assembleVecEdgeTrapPhiIntFuncContNu(g, markE0Tbdr, funcCont, N, qOrd, basesOnQuad)
+if iscell(funcCont)
+  ret = assembleVecEdgeTrapPhiIntFuncContNuVector(g, markE0Tbdr, funcCont, N, qOrd, basesOnQuad);
+else
+  ret = assembleVecEdgeTrapPhiIntFuncContNuScalar(g, markE0Tbdr, funcCont, N, qOrd, basesOnQuad);
+end % if
+end % function
+
+function ret = assembleVecEdgeTrapPhiIntFuncContNuVector(g, markE0Tbdr, funcCont, N, qOrd, basesOnQuad)
+K = g.numT;
+[Q, W] = quadRule1D(qOrd);
+ret = { zeros(K, N), zeros(K, N) };
+for n = 1 : 4
+  [Q1, Q2] = execin('darcyVert/gammaMapTrap', n, Q);
+  funcQ0E = { funcCont{1}(g.mapRef2Phy(1, Q1, Q2), g.mapRef2Phy(2, Q1, Q2)), ...
+              funcCont{2}(g.mapRef2Phy(1, Q1, Q2), g.mapRef2Phy(2, Q1, Q2)) };
+  for m = 1 : 2
+    markAreaNuE0T = markE0Tbdr(:, n) .* g.areaE0T(:, n) .* g.nuE0T(:, n, m);
+    for i = 1 : N
+      ret{m}(:, i) = ret{m}(:, i) + markAreaNuE0T .* ( funcQ0E{m} * (W.' .* basesOnQuad.phi1D(:, i, n)) );
+    end % for i
+  end  % for m
+end  % for n
+ret = cellfun(@(c) reshape(c.', K*N, 1), ret, 'UniformOutput', false);
+end % function
+
+function ret = assembleVecEdgeTrapPhiIntFuncContNuScalar(g, markE0Tbdr, funcCont, N, qOrd, basesOnQuad)
 K = g.numT;
 [Q, W] = quadRule1D(qOrd);
 ret = { zeros(K, N), zeros(K, N) };
