f05eb606af7210ac17c64c252092ee6d1182cf9d
Balthasar Reuter <balthasar.reuter@fau.de>
2017-03-16 17:55:34 +0100

    Fixed remaining errors. Testcases up to quadratic are solved correctly

--- configureProblem.m
+++ configureProblem.m
@@ -2,13 +2,13 @@ function problemData = configureProblem(problemData)
 
 %% Parameters.
 % Name of testcase
-problemData = setdefault(problemData, 'testcase', 'linear h');
+problemData = setdefault(problemData, 'testcase', 'convergence');
 
 % Number of elements in x- and y-direction
-problemData = setdefault(problemData, 'numElem', [8, 8]);
+problemData = setdefault(problemData, 'numElem', [6, 6]);
 
 % Local polynomial approximation order (0 to 4)
-problemData = setdefault(problemData, 'p', 1);
+problemData = setdefault(problemData, 'p', 2);
 
 % Order of quadrature rule
 problemData = setdefault(problemData, 'qOrd', 2*problemData.p + 1);
@@ -19,11 +19,11 @@ problemData = setdefault(problemData, 'tEnd', 0.1);  % end time
 problemData = setdefault(problemData, 'numSteps', 1);  % number of time steps
 
 % Visualization settings
-problemData = setdefault(problemData, 'isVisGrid', false);  % visualization of grid
-problemData = setdefault(problemData, 'isVisSol', true);  % visualization of solution
+problemData = setdefault(problemData, 'isVisGrid', true);  % visualization of grid
+problemData = setdefault(problemData, 'isVisSol', false);  % visualization of solution
 problemData = setdefault(problemData, 'outputFrequency', 10); % no visualization of every timestep
-problemData = setdefault(problemData, 'outputBasename', ...
-                ['output' filesep 'solution_sweVert_' problemData.testcase ]); % Basename of output files
+problemData = setdefault(problemData, 'outputBasename', ...  % Basename of output files
+                         ['output' filesep 'solution_sweVert_' problemData.testcase ]); 
 problemData = setdefault(problemData, 'outputTypes', { 'vtk' });  % Type of visualization files ('vtk, 'tec')
 
 %% Parameter check.
@@ -40,17 +40,19 @@ problemData.generateGrid = @(numElem) fn_domainRectTrap([0, domainWidth], [zBotC
 problemData.generateGrid1D = @(numElem, g2D) generateGridData1D([0, domainWidth], zBotConst + h0Const, numElem, g2D);
 
 % Boundary parts (0 = int, 1 = bot, 2 = right, 3 = top, 4 = left)
+checkMultipleIds = @(idE0T, ids) logical(sum(bsxfun(@eq, idE0T, reshape(ids, 1, 1, length(ids))), 3));
+
 problemData.generateMarkE0Tint = @(g) g.idE0T == 0;
 problemData.generateMarkE0TbdrBot = @(g) g.idE0T == 1;
 problemData.generateMarkE0TbdrTop = @(g) g.idE0T == 3;
-problemData.generateMarkE0TbdrLand = @(g) g.idE0T == idLand;
-problemData.generateMarkE0TbdrOS = @(g) g.idE0T == idOS;
-problemData.generateMarkE0TbdrRiv = @(g) g.idE0T == idRiv;
-problemData.generateMarkE0TbdrRad = @(g) g.idE0T == idRad;
+problemData.generateMarkE0TbdrLand = @(g) checkMultipleIds(g.idE0T, idLand);
+problemData.generateMarkE0TbdrOS = @(g) checkMultipleIds(g.idE0T, idOS);
+problemData.generateMarkE0TbdrRiv = @(g) checkMultipleIds(g.idE0T, idRiv);
+problemData.generateMarkE0TbdrRad = @(g) checkMultipleIds(g.idE0T, idRad);
 
 problemData.generateMarkV0T1Dint = @(g) g.idV0T == 0;
-problemData.generateMarkV0T1DbdrLand = @(g) g.idV0T == idLand;
-problemData.generateMarkV0T1DbdrOS = @(g) g.idV0T == idOS;
-problemData.generateMarkV0T1DbdrRiv = @(g) g.idV0T == idRiv;
-problemData.generateMarkV0T1DbdrRad = @(g) g.idV0T == idRad;
+problemData.generateMarkV0T1DbdrLand = @(g) checkMultipleIds(g.idV0T, idLand);
+problemData.generateMarkV0T1DbdrOS = @(g) checkMultipleIds(g.idV0T, idOS);
+problemData.generateMarkV0T1DbdrRiv = @(g) checkMultipleIds(g.idV0T, idRiv);
+problemData.generateMarkV0T1DbdrRad = @(g) checkMultipleIds(g.idV0T, idRad);
 end % function
