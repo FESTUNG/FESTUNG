a4a7e679460e66fce7c24a2a31672e1a31cd1c83
Balthasar Reuter <balthasar.reuter@fau.de>
2017-03-17 15:10:15 +0100

    Fixed error in Juw

--- sweVert/assembleMatEdgeTrapPhiIntFuncDiscIntNuPerQuad.m
+++ sweVert/assembleMatEdgeTrapPhiIntFuncDiscIntNuPerQuad.m
@@ -8,7 +8,7 @@ for n = 1 : 4
     ret{n, m} = sparse(K*N, K*R);
     markAreaNuE0T = markAreaE0T .* g.nuE0T(:, n, m);
     for j = 1 : N
-      ret{n} = ret{n} + kron(spdiags(markAreaNuE0T .* dataDisc(:, j), 0, K, K), refEdgePhiIntPhiIntPerQuad(:,j,:,n));
+      ret{n, m} = ret{n, m} + kron(spdiags(markAreaNuE0T .* dataDisc(:, j), 0, K, K), refEdgePhiIntPhiIntPerQuad(:,j,:,n));
     end % for j
   end % for m
 end % for n
