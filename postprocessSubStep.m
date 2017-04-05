56d60e968d1bdd42546f7f86518eb90577aa0c22
Balthasar Reuter <balthasar.reuter@fau.de>
2017-04-05 15:57:06 +0200

    Coupling restructured

--- postprocessSubStep.m
+++ postprocessSubStep.m
@@ -58,8 +58,10 @@ function problemData = postprocessSubStep(problemData, nStep, nSubStep)
 problemData.sweData = problemData.sweSteps.postprocessStep(problemData.sweData, (nStep - 1) * problemData.numSubSteps + nSubStep);
 problemData.sweData = problemData.sweSteps.outputStep(problemData.sweData, (nStep - 1) * problemData.numSubSteps + nSubStep);
 
-% Integrate water height over time for coupling (using trapezoidal rule)
-problemData.hSWE = problemData.hSWE + 0.5 * problemData.sweData.tau * (problemData.sweData.cDiscRK{1, 1} + problemData.sweData.cDiscRK{end, 1});
+if problemData.isCouplingDarcy
+  % Integrate water height over time for coupling (using trapezoidal rule)
+  problemData.hSWE = problemData.hSWE + 0.5 * problemData.sweData.tau * (problemData.sweData.cDiscRK{1, 1} + problemData.sweData.cDiscRK{end, 1});
+end % if
 
 problemData.isSubSteppingFinished = nSubStep >= problemData.numSubSteps;
 end % function
