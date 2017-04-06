258586cc4352c72eb3bd328dfbda2b6f6bac50cf
Balthasar Reuter <balthasar.reuter@fau.de>
2017-04-06 10:08:45 +0200

    Renamed adaptMesh to adaptFreeSurface

--- sweVert/preprocessStep.m
+++ sweVert/preprocessStep.m
@@ -2,5 +2,5 @@ function problemData = preprocessStep(problemData, nStep)
 problemData.cDiscRK(1, :) = problemData.cDiscRK(end, :);
 
 % Apply mesh adaptation to free surface movement
-problemData = problemData.fn_adaptMesh(problemData);
+problemData = problemData.fn_adaptFreeSurface(problemData);
 end % function
