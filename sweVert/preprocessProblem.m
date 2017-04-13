fb0f244a30ea07394cde49e7083d240c238c345e
Balthasar Reuter <balthasar.reuter@fau.de>
2017-04-13 15:14:15 +0200

    Restructured assembly algorithm to improve readability

--- sweVert/preprocessProblem.m
+++ sweVert/preprocessProblem.m
@@ -1,74 +1,114 @@
 function problemData = preprocessProblem(problemData)
 
-%% Triangulation.
-problemData.g = problemData.generateGrid(problemData.numElem);
-problemData.g.g1D = problemData.generateGrid1D(problemData.numElem(1), problemData.g);
-
-%% Additional mesh data
-% [K x 4] mark local edges that are horizontal or vertical
-problemData.g.markE0Th = [ true(problemData.g.numT, 2), false(problemData.g.numT, 2) ];
-problemData.g.markE0Tv = ~problemData.g.markE0Th;
-
-% [K x 4] mark local edges that are interior or have a certain boundary type
-problemData.g.markE0Tint = problemData.generateMarkE0Tint(problemData.g); 
-problemData.g.markE0Tbdr = ~problemData.g.markE0Tint; 
-problemData.g.markE0Tint = problemData.generateMarkE0Tint(problemData.g);
-problemData.g.markE0TbdrBot = problemData.generateMarkE0TbdrBot(problemData.g);
-problemData.g.markE0TbdrTop = problemData.generateMarkE0TbdrTop(problemData.g);
-problemData.g.markE0TbdrLand = problemData.generateMarkE0TbdrLand(problemData.g);
-problemData.g.markE0TbdrOS = problemData.generateMarkE0TbdrOS(problemData.g);
-problemData.g.markE0TbdrRiv = problemData.generateMarkE0TbdrRiv(problemData.g);
-problemData.g.markE0TbdrRad = problemData.generateMarkE0TbdrRad(problemData.g);
-problemData.g.markE0TbdrCoupling = problemData.generateMarkE0TbdrCoupling(problemData.g);
-problemData.g.markE0TbdrRiem = problemData.generateMarkE0TbdrRiem(problemData.g);
-
-% [barK x 2] mark local vertices that are interior or have a certain boundary type
-problemData.g.g1D.markV0Tint = problemData.generateMarkV0T1Dint(problemData.g.g1D);
-problemData.g.g1D.markV0Tbdr = ~problemData.g.g1D.markV0Tint;
-problemData.g.g1D.markV0TbdrLand = problemData.generateMarkV0T1DbdrLand(problemData.g.g1D);
-problemData.g.g1D.markV0TbdrOS = problemData.generateMarkV0T1DbdrOS(problemData.g.g1D);
-problemData.g.g1D.markV0TbdrRiv = problemData.generateMarkV0T1DbdrRiv(problemData.g.g1D);
-problemData.g.g1D.markV0TbdrRad = problemData.generateMarkV0T1DbdrRad(problemData.g.g1D);
-problemData.g.g1D.markV0TbdrRiem = problemData.generateMarkV0T1DbdrRiem(problemData.g.g1D);
-
-% prescribed horizontal velocity
-problemData.g.markE0TbdrU = problemData.g.markE0TbdrLand | problemData.g.markE0TbdrBot | problemData.g.markE0TbdrCoupling;
-% prescribed vertical velocity
-problemData.g.markE0TbdrW = problemData.g.markE0TbdrBot | problemData.g.markE0TbdrCoupling;
-% prescribed water height
-problemData.g.markE0TbdrH = problemData.g.markE0TbdrOS | problemData.g.markE0TbdrRiv;
-% prescribed diffusion
-problemData.g.markE0TbdrQ = problemData.g.markE0TbdrTop | problemData.g.markE0TbdrOS | problemData.g.markE0TbdrRad;
-% prescribed flow rate
-problemData.g.markE0TbdrUH = problemData.g.markE0TbdrRiv;
-problemData.g.g1D.markV0TbdrH = problemData.g.g1D.markV0TbdrOS | problemData.g.g1D.markV0TbdrRiv;
-problemData.g.g1D.markV0TbdrUH = problemData.g.g1D.markV0TbdrRiv;
-
-%% Function handles for problem-specific functions
-problemData.fn_adaptFreeSurface = getFunctionHandle([problemData.problemName filesep 'adaptFreeSurface']);
-problemData.fn_assembleJumpTerms = getFunctionHandle([problemData.problemName filesep 'assembleJumpTerms']);
-
-problemData.fn_assembleMatEdgeTetraHorizPhiPhiNuBottomUp = getFunctionHandle([problemData.problemName filesep 'assembleMatEdgeTetraHorizPhiPhiNuBottomUp']);
-problemData.fn_assembleMatEdgeTetraPhiPhiFuncDisc1DNuHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatEdgeTetraPhiPhiFuncDisc1DNuHeight']);
-problemData.fn_assembleMatElem1DDphiPhiFuncDiscHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatElem1DDphiPhiFuncDiscHeight']);
-problemData.fn_assembleMatV0T1DPhiPhiFuncDiscNuHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatV0T1DPhiPhiFuncDiscNuHeight']);
-problemData.fn_assembleMatV0T1DPhiIntPhiIntFuncDiscIntNuHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatV0T1DPhiIntPhiIntFuncDiscIntNuHeight']);
-problemData.fn_assembleVecEdgeTetraPhiIntFuncContHeightNu = getFunctionHandle([problemData.problemName filesep 'assembleVecEdgeTetraPhiIntFuncContHeightNu']);
-
 %% Globally constant parameters.
 problemData.N = (problemData.p + 1)^2;  % number of local DOFs on trapezoidals
 problemData.barN = problemData.p + 1;  % number of local DOFs on intervals
 problemData.tau = (problemData.tEnd - problemData.t0) / problemData.numSteps;  % time step size
 
+%% Triangulation.
+problemData.g = problemData.generateGrid(problemData.numElem);
+problemData.g.g1D = problemData.generateGrid1D(problemData.numElem(1), problemData.g);
+
+%% Additional 2D mesh data: [K x 4] marker for local edges
+% Edges that are horizontal or vertical
+problemData.g.markE0Th = [ true(problemData.g.numT, 2), false(problemData.g.numT, 2) ];
+problemData.g.markE0Tv = ~problemData.g.markE0Th;
+
+% Interior edges
+problemData.g.markE0Tint = problemData.generateMarkE0Tint(problemData.g); 
+
+% Boundary edges
+problemData.g.markE0Tbdr = sparse(~problemData.g.markE0Tint); 
+
+% Bottom boundary edges
+problemData.g.markE0TbdrBot = sparse(problemData.generateMarkE0TbdrBot(problemData.g));
+
+% Top boundary edges
+problemData.g.markE0TbdrTop = sparse(problemData.generateMarkE0TbdrTop(problemData.g));
+
+% Land, Open sea, River and Radiation boundary edges
+markE0TbdrLand = problemData.generateMarkE0TbdrLand(problemData.g);
+markE0TbdrOS = problemData.generateMarkE0TbdrOS(problemData.g);
+markE0TbdrRiv = problemData.generateMarkE0TbdrRiv(problemData.g);
+markE0TbdrRad = problemData.generateMarkE0TbdrRad(problemData.g);
+
+% Coupling boundary edges
+problemData.g.markE0TbdrCoupling = sparse(problemData.generateMarkE0TbdrCoupling(problemData.g));
+
+% Boundary edges with Riemann solver applied
+problemData.g.markE0TbdrRiem = problemData.generateMarkE0TbdrRiem(problemData.g);
+
+% Prescribed horizontal velocity without and with Riemann solver
+problemData.g.markE0TbdrU = sparse(~(problemData.g.markE0TbdrRiem | problemData.g.markE0TbdrCoupling) & (markE0TbdrLand | problemData.g.markE0TbdrBot));
+problemData.g.markE0TbdrRiemU = sparse(problemData.g.markE0TbdrRiem & ~problemData.g.markE0TbdrCoupling & (markE0TbdrLand | problemData.g.markE0TbdrBot));
+
+% Prescribed vertical velocity without Riemann solver
+problemData.g.markE0TbdrW = sparse(problemData.g.markE0TbdrBot & ~problemData.g.markE0TbdrCoupling);
+
+% Prescribed water height without and with Riemann solver
+problemData.g.markE0TbdrH = sparse(~problemData.g.markE0TbdrRiem & (markE0TbdrOS | markE0TbdrRiv));
+problemData.g.markE0TbdrRiemH = sparse(problemData.g.markE0TbdrRiem & (markE0TbdrOS | markE0TbdrRiv));
+
+% Prescribed diffusion without Riemann solver
+problemData.g.markE0TbdrQ = sparse(problemData.g.markE0TbdrTop | markE0TbdrOS | markE0TbdrRad);
+
+% Prescribed flow rate without and with Riemann solver
+problemData.g.markE0TbdrUH = sparse(~problemData.g.markE0TbdrRiem & markE0TbdrRiv);
+problemData.g.markE0TbdrRiemUH = sparse(problemData.g.markE0TbdrRiem & markE0TbdrRiv);
+
+%% Additional 1D mesh data: [barK x 2] marker for local edges
+% Interior vertices
+problemData.g.g1D.markV0Tint = problemData.generateMarkV0T1Dint(problemData.g.g1D);
+
+% Boundary vertices
+problemData.g.g1D.markV0Tbdr = sparse(~problemData.g.g1D.markV0Tint);
+
+% Land, Open sea, River and Radiation boundary vertices
+markV0TbdrLand = problemData.generateMarkV0T1DbdrLand(problemData.g.g1D);
+markV0TbdrOS = problemData.generateMarkV0T1DbdrOS(problemData.g.g1D);
+markV0TbdrRiv = problemData.generateMarkV0T1DbdrRiv(problemData.g.g1D);
+% markV0TbdrRad = problemData.generateMarkV0T1DbdrRad(problemData.g.g1D);
+
+% Boundary vertices with Riemann solver applied
+problemData.g.g1D.markV0TbdrRiem = problemData.generateMarkV0T1DbdrRiem(problemData.g.g1D);
+
+% Prescribed flow rate without and with Riemann solver
+problemData.g.g1D.markV0TbdrUH = sparse(~problemData.g.g1D.markV0TbdrRiem & markV0TbdrRiv);
+problemData.g.g1D.markV0TbdrRiemUH = sparse(problemData.g.g1D.markV0TbdrRiem & markV0TbdrRiv);
+
+% Prescribed water height without and with Riemann solver
+problemData.g.g1D.markV0TbdrH = sparse(~problemData.g.g1D.markV0TbdrRiem & ~markV0TbdrRiv & markV0TbdrOS);
+problemData.g.g1D.markV0TbdrRiemH = sparse(problemData.g.g1D.markV0TbdrRiem & ~markV0TbdrRiv & markV0TbdrOS);
+
+% Prescribed horizontal velocity without and with Riemann solver
+problemData.g.g1D.markV0TbdrU = sparse(~problemData.g.g1D.markV0TbdrRiem & ~markV0TbdrRiv & markV0TbdrLand);
+problemData.g.g1D.markV0TbdrRiemU = sparse(problemData.g.g1D.markV0TbdrRiem & ~markV0TbdrRiv & markV0TbdrLand);
+
+assert(~any((problemData.g.markE0TbdrRiemU(:) & ~problemData.g.markE0TbdrRiemH(:)) | (~problemData.g.markE0TbdrRiemU(:) & problemData.g.markE0TbdrRiemH(:))), ...
+      'Only u or h given with Riemann solver is not yet implemented in continuity equation!');
+  
+assert(~any(problemData.g.g1D.markV0TbdrU(:) | problemData.g.g1D.markV0TbdrRiemU(:) | problemData.g.g1D.markV0TbdrH(:) | problemData.g.g1D.markV0TbdrRiemH(:)), ...
+      'Only u or h given is not yet implemented in free-surface equation!');
+
+%% Function handles for problem-specific functions
+problemData.fn_adaptFreeSurface = getFunctionHandle([problemData.problemName filesep 'adaptFreeSurface']);
+problemData.fn_assembleMatEdgeTetraHorizPhiPhiNuBottomUp = getFunctionHandle([problemData.problemName filesep 'assembleMatEdgeTetraHorizPhiPhiNuBottomUp']);
+problemData.fn_assembleMatEdgeTetraPhiPhiFuncDisc1DNuHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatEdgeTetraPhiPhiFuncDisc1DNuHeight']);
+problemData.fn_assembleMatEdgeTetraPhiIntPhiIntFuncDisc1DIntNuHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatEdgeTetraPhiIntPhiIntFuncDisc1DIntNuHeight']);
+problemData.fn_assembleMatElem1DDphiPhiFuncDiscHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatElem1DDphiPhiFuncDiscHeight']);
+problemData.fn_assembleMatV0T1DPhiPhiFuncDiscNuHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatV0T1DPhiPhiFuncDiscNuHeight']);
+problemData.fn_assembleMatV0T1DPhiIntPhiIntFuncDiscIntNuHeight = getFunctionHandle([problemData.problemName filesep 'assembleMatV0T1DPhiIntPhiIntFuncDiscIntNuHeight']);
+problemData.fn_assembleVecEdgeTetraPhiIntFuncContHeightNu = getFunctionHandle([problemData.problemName filesep 'assembleVecEdgeTetraPhiIntFuncContHeightNu']);
+
 %% Configuration output.
 fprintf('-------------------------------------------------------------------------------------------\n');
-fprintf('Running testcase "%s".\n', problemData.testcase);
+fprintf('Running problem "%s" with testcase "%s".\n', problemData.problemName, problemData.testcase);
 fprintf('Computing with polynomial order %d (%d local DOFs) on %d x %d (%d) trapezoids.\n', ...
         problemData.p, problemData.N, problemData.numElem(1), problemData.numElem(2), problemData.g.numT);
 fprintf('%d time steps from t = %g to %g.\n', problemData.numSteps, problemData.t0, problemData.tEnd);
 fprintf('-------------------------------------------------------------------------------------------\n');
 
-%% Lookup table for basis function.
+%% Lookup tables for basis function.
 problemData.basesOnQuad1D = computeBasesOnQuad1D(problemData.p, struct, [problemData.qOrd, problemData.qOrd+1]);
 problemData.basesOnQuad2D = computeBasesOnQuadTensorProduct(problemData.p, struct, [problemData.qOrd, problemData.qOrd+1]);
 
@@ -95,7 +135,7 @@ problemData.tildeHatQoffdiag = integrateRefEdgeTetraPhiIntPhi1DExt([problemData.
 problemData.tildeHatPdiag = integrateRefEdgeTetraPhiIntPhiIntPhi1DInt([problemData.N problemData.barN], problemData.qOrd, problemData.basesOnQuad2D, problemData.basesOnQuad1D);
 problemData.tildeHatPoffdiag = integrateRefEdgeTetraPhiIntPhiExtPhi1DExt([problemData.N problemData.barN], problemData.qOrd, problemData.basesOnQuad2D, problemData.basesOnQuad1D);
 
-%% Computation of time-independent 1D matrices.
+%% One-dimensional mass matrix in free-surface equation (XII)
 problemData.barGlobM = assembleMatElemPhiPhi(problemData.g.g1D, problemData.barHatM);
 
 %% Empty vectors and matrices for coupled problem
