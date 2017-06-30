f43f6e71806be21472c558ea1cc52ef4a24a0b27
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-06-30 16:49:35 +0200

    fix to determine local solve block size to work for numbers not divisible by 16

--- determineLocalSolveBlockSize.m
+++ determineLocalSolveBlockSize.m
@@ -2,8 +2,15 @@ function localSolveBlockSize = determineLocalSolveBlockSize( K )
     assert( K > 0, 'The mesh must have at least one element');
     localSolveBlockSize = min(K, 16);
     if (mod(K, localSolveBlockSize) ~= 0)
-        localSolveBlockSize = 1;
-        warning('Block size does not fit the problem. Reset block size to 1');
+      
+      if ( K < 16 )
+        localSolveBlockSize = K;
+      else
+        localSolveBlockSize = K / floor(K/16);
+      end
+        
+%         localSolveBlockSize = 1;
+%         warning('Block size does not fit the problem. Reset block size to 1');
     end
     assert( mod(K, localSolveBlockSize) == 0, ...
         'Block size does not fit to problem size!');
