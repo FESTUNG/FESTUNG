4ff7a19d25c4dc8c83a1ce9adbb81c73a12c650e
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-07-03 11:40:17 +0200

    reverted determinLocalSolveBlockSize to old revisio because the new one may give non-integer block sizes

--- determineLocalSolveBlockSize.m
+++ determineLocalSolveBlockSize.m
@@ -3,15 +3,21 @@ function localSolveBlockSize = determineLocalSolveBlockSize( K )
     localSolveBlockSize = min(K, 16);
     if (mod(K, localSolveBlockSize) ~= 0)
       
-      if ( K < 16 )
-        localSolveBlockSize = K;
-      else
-        localSolveBlockSize = K / floor(K/16);
-      end
+%       if ( K < 16 )
+%         localSolveBlockSize = K;
+%       else
+%         localSolveBlockSize = K / floor(K/16-0.5);
+%       end
         
-%         localSolveBlockSize = 1;
-%         warning('Block size does not fit the problem. Reset block size to 1');
+        localSolveBlockSize = 1;
+        warning('Block size does not fit the problem. Reset block size to 1');
     end
+    
+%     if (mod(K, localSolveBlockSize) ~= 0)
+%         localSolveBlockSize = 1;
+%         warning('Block size does not fit the problem. Reset block size to 1');    
+%     end
+    
     assert( mod(K, localSolveBlockSize) == 0, ...
         'Block size does not fit to problem size!');
 end
