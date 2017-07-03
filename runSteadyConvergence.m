b8f37f5cd780aa84ebb3e31d2ba47946d6051ed0
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-07-03 12:21:50 +0200

    update on convergence run script

--- runSteadyConvergence.m
+++ runSteadyConvergence.m
@@ -15,36 +15,57 @@ err = 1e10 * ones( pMax+1, jMax+1 );
 
 for p=0:pMax
   problemData.p = p;
+  
+  filename = strcat('p', num2str(p), '.txt');
+  fileID = fopen(filename,'w');
+  writeReportHeader(fileID);
+  
   for j=0:jMax
     fprintf('Polynomial degree: %d, mesh level: %d \n', p, j );
     problemData.hmax = vecHmax(j+1) ;
     
     resultProblemData = main('hdg_advection_steady', problemData);
     err(p+1, j+1) = resultProblemData.L2error;
+    
+    fprintf('Polynomial degree: %d\n', p);
+    generateCurrentReport( 1, p, j, vecHmax, err );
+    generateCurrentReport( fileID, p, j, vecHmax, err );
   end
-
-  fprintf('Polynomial degree: %d\n', p);
-  generateReport( 1, p, jMax, vecHmax, err );
-  
-  filename = strcat('p', num2str(p), '.txt');
-  fileID = fopen(filename,'w');
-  generateReport( fileID, p, jMax, vecHmax, err );
   fclose(fileID);
 end
 
 end
 
-function generateReport( fileID, p, jMax, vecHmax, err )
 
-
+function writeReportHeader( fileID )
 %   Write header
 fprintf(fileID, 'lvl \tNel \terr \torder\n');
+end
+
+function writeReportLine( fileID, j, ne, err, ord )
+%   Write line
+  fprintf(fileID, '%d \t%d \t%1.10f \t%1.10f \n', j, ne, err, ord );
+end
+
+function generateCurrentReport( fileID, p, j, vecHmax, err )
+%   Write header
+% writeReportHeader(fileID, 'lvl \tNel \terr \torder\n');
+ord = 0.;
+if j>0
+  ord = log(err(p+1, j)/err(p+1, j+1))/log(2) ;
+end 
+writeReportLine( fileID, j, 1/vecHmax(j+1)*1/vecHmax(j+1)*2, err(p+1, j+1), ord );
+end
+
+function generateReport( fileID, p, jMax, vecHmax, err )
+%   Write header
+writeReportHeader(fileID, 'lvl \tNel \terr \torder\n');
 for j=0:jMax
+  ord = 0.;
   if j>0
-    fprintf(fileID, '%d \t%d \t%g \t%g \n', j, 1/vecHmax(j+1)*1/vecHmax(j+1)*2, err(p+1, j+1), log(err(p+1, j)/err(p+1, j+1))/log(2) );
-  else
-    fprintf(fileID, '%d \t%d \t%g \t0.0 \n', j, 1/vecHmax(j+1)*1/vecHmax(j+1)*2, err(p+1, j+1) );
-  end
+    ord = log(err(p+1, j)/err(p+1, j+1))/log(2) ;
+  end 
+  writeReportLine( fileID, j, 1/vecHmax(j+1)*1/vecHmax(j+1)*2, err(p+1, j+1), ord );
 end 
 
 end
