575c6a6249256fcbc0669404f9c9404061f1f009
Alexander Jaust <alexander.jaust@rwth-aachen.de>
2017-07-10 10:03:16 +0200

    forgor to add getTestcase for steady hdg code

---
+++ hdg_advection_steady/getTestcase.m
@@ -0,0 +1,37 @@
+function problemData = getTestcase(problemData, problemName)
+switch problemName
+  case 'Convergence'
+    getLinearAdvectionSol = @(x1, x2) cos(7*x1).*cos(7*x2);
+    cCont  = @(t,x1,x2) cos(7*x1).*cos(7*x2);
+    c0Cont = @(x1,x2) cCont(0,x1,x2);
+    cDCont = @(x1,x2) cCont(0,x1,x2);
+    fCont  = @(x1,x2) -7*sin(7*x1).*cos(7*x2).*exp((x1+x2)/2) ...
+      -7*cos(7*x1).*sin(7*x2).*exp((x1-x2)/2) ...
+      + 0.5*cos(7*x1).*cos(7*x2).*exp((x1+x2)/2) ...
+      - 0.5*cos(7*x1).*cos(7*x2).*exp((x1-x2)/2);
+    fluxCont = @(x1, x2, c ) evalSteadyFlux(0, x1, x2, c);
+    
+    generateMarkE0TbdrN = @(g) generateSteadyOutflowBoundary(g);
+    generateMarkE0TbdrD = @(g) ~(g.markE0Tint | g.markE0TbdrN);
+    
+    generateGridData = @(hmax) domainArbitrarySquare( 0.0, 1.0, hmax );
+    
+  otherwise
+    error('Specified test case is not available. Please check your configuration.');
+
+
+end % switch
+
+problemData = setdefault(problemData, 'c0Cont', c0Cont);
+problemData = setdefault(problemData, 'cCont', cCont);
+problemData = setdefault(problemData, 'fCont', fCont);
+problemData = setdefault(problemData, 'fluxCont', fluxCont);
+problemData = setdefault(problemData, 'cDCont', cDCont);
+problemData = setdefault(problemData, 'getLinearAdvectionSol', getLinearAdvectionSol);
+problemData = setdefault(problemData, 'generateMarkE0TbdrN', generateMarkE0TbdrN);
+problemData = setdefault(problemData, 'generateMarkE0TbdrD', generateMarkE0TbdrD);
+
+problemData = setdefault(problemData, 'generateGridData', generateGridData);
+
+
+end % function
