47573ced4ae41353ec26758a462c9f73f1b3f2b8
Balthasar Reuter <balthasar.reuter@fau.de>
2017-08-04 23:56:53 +0200

    Add code version for third publication.
    This introduces some major changes to the code structure:
    - Solvers for problems (e.g., advection, diffusion) are now implmented following a fixed solver framework.
    - Each problem implements a number of steps as Matlab functions that are stored in a common subfolder.
    - Routine main() is now the single point of entry, expecting the name of the problem (i.e., folder name).
    - Existing operators advection/diffusion have been converted to this new structure.
    - New hybridized formulation of linear advection is added (as described in the third paper in series).
    - New implicit advection (using DIRK schemes) added.

---
+++ configureProblem.m
@@ -0,0 +1,106 @@
+% Fills the problemData-struct with all basic configuration options.
+% Problem parameters are to be modified inside this routine.
+
+%===============================================================================
+%> @file advection_implicit/configureProblem.m
+%>
+%> @brief Fills the problemData-struct with all basic configuration options.
+%>        Problem parameters are to be modified inside this routine.
+%===============================================================================
+%>
+%> @brief Fills the problemData-struct with all basic configuration options.
+%>        Problem parameters are to be modified inside this routine.
+%>
+%> This routine is called before any other function for the problem.
+%> It defines all problem parameters and should be the only file users have
+%> to adapt to their needs.
+%> 
+%> It provides all configuration options for the numerical solver to 
+%> approximate solutions 
+%> @f$c:\overline{J}\times\overline{\Omega}\rightarrow\mathbb{R}@f$ 
+%> of the advection equation
+%> @f{align*}{
+%> \partial_t c  + \nabla\cdot (\mathbf{u}\,c) &\;=\; f            &&\text{in}~J\times\Omega\,,\\
+%> c                                           &\;=\; c_\mathrm{D} &&\text{on}~J\times{\partial\Omega}_{\mathrm{in}}\,,\\
+%> c                                           &\;=\; c^0          &&\text{on}~\{0\}\times\Omega\,.
+%> @f}
+%> The velocity @f$\mathbf{u}:J\times\Omega\rightarrow\mathbb{R}^2@f$ and 
+%> right hand side@f$f:J\times\Omega\rightarrow \mathbb{R}@f$
+%> may vary in time and space. 
+%> A detailed description can be found in @ref RAWFK2016. However, this
+%> version uses an implicit discretization in time and no slope limiting.
+%>
+%> Please read the inline-comments in the code for the meaning of each
+%> configuration option.
+%>
+%> @param  problemData  A (probably) empty struct with problem parameters.
+%>                      @f$[\text{struct}]@f$
+%>
+%> @retval problemData  A struct with all necessary parameters and definitions
+%>                      for the problem description. @f$[\text{struct}]@f$
+%>
+%> This file is part of FESTUNG
+%>
+%> @copyright 2014-2017 Balthasar Reuter, Florian Frank, Vadym Aizinger
+%> 
+%> @par License
+%> @parblock
+%> This program is free software: you can redistribute it and/or modify
+%> it under the terms of the GNU General Public License as published by
+%> the Free Software Foundation, either version 3 of the License, or
+%> (at your option) any later version.
+%>
+%> This program is distributed in the hope that it will be useful,
+%> but WITHOUT ANY WARRANTY; without even the implied warranty of
+%> MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%> GNU General Public License for more details.
+%>
+%> You should have received a copy of the GNU General Public License
+%> along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%> @endparblock
+%
+function problemData = configureProblem(problemData)
+%% Parameters.
+% Choose testcase
+problemData = setdefault(problemData, 'testcase', 'solid_body');
+
+% Mark run as convergence test (enforces Friedrichs-Keller triangulation)
+problemData = setdefault(problemData, 'isConvergence', false);
+
+% Maximum edge length of triangle
+problemData = setdefault(problemData, 'hmax', 2^-6);
+
+% Local polynomial approximation order (0 to 4)
+problemData = setdefault(problemData, 'p', 2);
+
+% Order of Runge-Kutta method
+problemData = setdefault(problemData, 'ordRK', min(problemData.p + 1, 4));
+
+% Order of quadrature rule
+problemData = setdefault(problemData, 'qOrd', 2*problemData.p + 1);
+
+% Load testcase
+problemData = execin([ problemData.problemName filesep 'getTestcase' ], problemData, problemData.testcase);
+
+% Visualization settings
+problemData = setdefault(problemData, 'isVisGrid', false);  % visualization of grid
+problemData = setdefault(problemData, 'isVisSol', true);  % visualization of solution
+problemData = setdefault(problemData, 'outputFrequency', 50); % no visualization of every timestep
+problemData = setdefault(problemData, 'outputBasename', ...
+                         ['output' filesep 'advection_implicit']); 
+problemData = setdefault(problemData, 'outputTypes', { 'vtk', 'tec' });  % Type of visualization files ('vtk, 'tec')
+%% Parameter check.
+assert(problemData.p >= 0 && problemData.p <= 4, 'Polynomial order must be zero to four.')
+assert(problemData.ordRK >= 1 && problemData.ordRK <= 4, 'Order of Runge-Kutta method must be zero to four.')
+assert(problemData.hmax > 0, 'Maximum edge length must be positive.')
+assert(problemData.numSteps > 0, 'Number of time steps must be positive.')
+%% Domain and triangulation configuration.
+% Triangulate unit square using pdetool (if available or Friedrichs-Keller otherwise).
+if ~problemData.isConvergence && license('checkout','PDE_Toolbox')
+  problemData.generateGridData = @(hmax) domainPolygon([0 1 1 0], [0 0 1 1], hmax);
+else
+  fprintf('PDE_Toolbox not available. Using Friedrichs-Keller triangulation.\n');
+  problemData.generateGridData = @domainSquare;
+end % if
+end % function
+
