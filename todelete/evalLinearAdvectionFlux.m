72d05f5b5a66b205342bf3bf51fc44cd91464fec
Balthasar Reuter <balthasar.reuter@fau.de>
2017-09-21 13:49:56 +0200

    removed todelete

--- todelete/evalLinearAdvectionFlux.m
+++
@@ -1,5 +0,0 @@
-function [fx, fy] = evalLinearAdvectionFlux( t, x1, x2, c)
-assert(isequal(size(x1), size(x2)), 'x1 and x2 must have the same size')
-fx = 1 .* c;
-fy = 1 .* c;
-end
--- todelete/evalSteadyFlux.m
+++
@@ -1,5 +0,0 @@
-function [fx, fy] = evalSteadyFlux( t, x1, x2, c)
-assert(isequal(size(x1), size(x2)), 'x1 and x2 must have the same size')
-fx = exp((x1+x2)/2) .* c;
-fy = exp((x1-x2)/2) .* c;
-end
