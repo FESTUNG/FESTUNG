d9348537e80356b5ca3d08d294365444f3e0f02a
Balthasar Reuter <balthasar.reuter@fau.de>
2017-09-21 13:49:26 +0200

    removed codeNew

--- codeNew/assembleMatElemDphiPerQuad.m
+++
@@ -1,6 +0,0 @@
-function ret = assembleMatElemDphiPerQuad(g, refElemDphiPerQuad)
-K = g.numT;
-ret = cell(2,1);
-ret{1} = kron(spdiags(g.B(:,2,2), 0, K, K), refElemDphiPerQuad(:,:,1)) - kron(spdiags(g.B(:,2,1), 0, K, K), refElemDphiPerQuad(:,:,2));
-ret{2} = kron(spdiags(g.B(:,1,1), 0, K, K), refElemDphiPerQuad(:,:,2)) - kron(spdiags(g.B(:,1,2), 0, K, K), refElemDphiPerQuad(:,:,1));
-end % function
