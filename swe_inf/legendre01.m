0c6dfd423b5bc061f8e8066258ab17dd8e26f841
Balthasar Reuter <balthasar.reuter@fau.de>
2017-09-22 16:46:21 +0200

    swe_inf moved to submodule

--- swe_inf/legendre01.m
+++
@@ -1,9 +0,0 @@
-function ret = legendre01(i,X)
-switch i
-  case 1,  ret = ones(size(X));
-  case 2,  ret = sqrt(12)   .*(                                X - 0.5 );
-  case 3,  ret = sqrt(180)  .*(                     X.^2 -     X + 1/6 );
-  case 4,  ret = sqrt(2800) .*(          X.^3 - 3/2*X.^2 + 3/5*X - 1/20);  
-  case 5,  ret = sqrt(44100).*(X.^4 - 2.*X.^3 + 9/7*X.^2 - 2/7*X + 1/70);
-end % switch
-end % function
--- swe_inf/phiLin.m
+++
@@ -1,6 +0,0 @@
-function ret = phiLin(i, X1, X2)
-switch i
-  case 1,  ret = ones(size(X1));  
-  case 2,  ret = X2;
-end % switch
-end % function
