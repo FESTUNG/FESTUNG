ff4ffcebc8213531a3e950d39cc0a7de80efa74e
Balthasar Reuter <balthasar.reuter@fau.de>
2017-09-22 16:39:03 +0200

    2Dv coupled problem moved to submodules

--- sweVert/preprocessStep.m
+++
@@ -1,6 +0,0 @@
-function problemData = preprocessStep(problemData, nStep)
-problemData.cDiscRK(1, :) = problemData.cDiscRK(end, :);
-
-% Apply mesh adaptation to free surface movement
-problemData = problemData.fn_adaptFreeSurface(problemData);
-end % function
