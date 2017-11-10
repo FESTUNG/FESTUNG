710388fd4e7312104ee2b05502cc34457a37fa72
Balthasar Reuter <balthasar.reuter@fau.de>
2017-11-10 18:00:59 +0100

    Mixed boundary terms (Dirichlet h but not u) added

---
+++ assembleVecV0T1DPhiIntFuncDiscIntNuHeight.m
@@ -0,0 +1,9 @@
+function ret = assembleVecV0T1DPhiIntFuncDiscIntNuHeight(g1D, markV0T, dataDisc, heightV0T1D, barN, qOrd, basesOnQuad)
+barK = g1D.numT;
+ret = zeros(barK, barN);
+for n = 1 : 2
+  funcDiscV0T = dataDisc * basesOnQuad.phi0D{qOrd}(:, n);
+  ret = ret + (markV0T(:, n) .* g1D.nuV0T(:, n) .* funcDiscV0T ./ heightV0T1D(:, n)) * basesOnQuad.phi0D{qOrd}(:, n).';
+end  % for n
+ret = reshape(ret.', barK*barN, 1);
+end  % function
