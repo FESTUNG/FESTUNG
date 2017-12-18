9cb567bd4be4355770e2a606ba7bfd2840a5947a
Balthasar Reuter <balthasar.reuter@fau.de>
2017-12-18 16:34:45 +0100

    Vector assembly unified

--- preprocessStep.m
+++ preprocessStep.m
@@ -4,28 +4,28 @@ t = nStep * problemData.tau;
 
 %% L2-projections of algebraic coefficients.
 KDisc = cellfun(@(c) projectFuncCont2DataDiscTetra(problemData.g, @(x1,x2) c(t,x1,x2), problemData.qOrd, ...
-                       problemData.globM, problemData.basesOnQuad), problemData.KCont, 'UniformOutput', false);
+          problemData.globM, problemData.basesOnQuad), problemData.KCont, 'UniformOutput', false);
 fDisc = projectFuncCont2DataDiscTetra(problemData.g, @(x1,x2) problemData.fCont(t,x1,x2), problemData.qOrd, ...
           problemData.globM, problemData.basesOnQuad);
                              
 %% Assembly of time-dependent global matrices.
 problemData.globG = assembleMatElemDphiPhiFuncDisc(problemData.g, problemData.hatG, KDisc);
 problemData.globR = assembleMatEdgePhiPhiFuncDiscNu(problemData.g, problemData.g.markE0Tint, ...
-                    problemData.hatRdiag, problemData.hatRoffdiag, KDisc);
+                      problemData.hatRdiag, problemData.hatRoffdiag, KDisc);
 
 %% Assembly of Dirichlet boundary contributions.
 hDCont = @(x1,x2) problemData.hDCont(t,x1,x2);
 problemData.globRD = assembleMatEdgePhiIntPhiIntFuncDiscIntNu(problemData.g, ...
-                     problemData.g.markE0TbdrD | problemData.g.markE0TbdrCoupling, problemData.hatRdiag, KDisc);
+                      problemData.g.markE0TbdrD | problemData.g.markE0TbdrCoupling, problemData.hatRdiag, KDisc);
 problemData.globJD = assembleVecEdgeTetraPhiIntFuncContNu(problemData.g, problemData.g.markE0TbdrD, ...
-                     hDCont, problemData.N, problemData.qOrd, problemData.basesOnQuad);
-problemData.globKD = assembleVecEdgeTetraPhiIntFuncCont(problemData.g, problemData.g.markE0TbdrD, ...
-                     hDCont, problemData.N, problemData.qOrd, problemData.basesOnQuad, ones(problemData.g.numT, 4));
+                      hDCont, problemData.N, problemData.qOrd, problemData.basesOnQuad);
+problemData.globKD = assembleVecEdgePhiIntFuncCont(problemData.g, problemData.g.markE0TbdrD, ...
+                      hDCont, problemData.N, problemData.basesOnQuad, problemData.qOrd, ones(problemData.g.numT, 4));
                   
 %% Assembly of Neumann boundary contributions.
 gNCont = @(x1,x2) problemData.gNCont(t,x1,x2);
-problemData.globKN = assembleVecEdgeTetraPhiIntFuncCont(problemData.g, ...
-                     problemData.g.markE0TbdrN, gNCont, problemData.N, problemData.qOrd, problemData.basesOnQuad);
+problemData.globKN = assembleVecEdgePhiIntFuncCont(problemData.g, ...
+                     problemData.g.markE0TbdrN, gNCont, problemData.N, problemData.basesOnQuad, problemData.qOrd);
                    
 %% Assembly of the source contribution.
 problemData.globL = problemData.globM * reshape(fDisc', problemData.g.numT * problemData.N, 1);
