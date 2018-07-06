30c0229b96102e0fcb27b6f2ef913828b1ae56cc
Flo Frank <florian.frank@fau.de>
2018-07-06 09:14:53 +0200

    Option for Voronoi grid added to domainSquare.

--- domainSquare.m
+++ domainSquare.m
@@ -3,37 +3,45 @@
 %===============================================================================
 %> @file
 %>
-%> @brief Generates a mesh for a square.
+%> @brief Generates a Friedrichs-Keller triangulation on a square.
 %===============================================================================
 %>
-%> @brief Generates a mesh for a square.
+%> @brief Generates a Friedrichs-Keller triangulation on a square.
 %>
-%> This function allows to generate a Friedrich-Keller triangulation for 
-%> a square @f$\{(a,a), (b,a), (b,b), (a,b)\}@f$.
-%>
-%> If bounds for @f$x, y@f$ are not specified, it defaults to the unit
+%> This function generates a Friedrichs-Keller triangulation on a
+%> square-bounded domain @f$\{(a,a), (b,a), (b,b), (a,b)\}@f$.
+%> If no bounds for @f$x, y@f$ are specified, it defaults to the unit
 %> square @f$[0,1]^2@f$.
+%> Optionally, the mesh can be transformed such that every element becomes
+%> an equilateral triangle with the same maximum edge length h.  This mesh
+%> is a Voronoi grid.  The square-shaped boundary becomes a parallelogram
+%> in this case.
+%> 
 %>
 %> @par Example
 %> @parblock
 %> @code
-%> g = domainSquare(1/3);
-%> visualizeGrid(g)
+%> g1 = domainSquare(1/3);
+%> visualizeGrid(g1)
+%> g2 = domainSquare(1/3, 0, 1, true);
+%> visualizeGrid(g2)
 %> @endcode
 %> produces the following output:
-%> @image html  domainSquare.png
+%> @image html  domainSquare.png  Grid g1
+%> @image html  domainSquareEquilateral.png   Grid g2 (isEquilateral = true)
 %> @endparblock
 %>
 %> @param  h      The maximum diameter of an element.
 %> @param  a     (optional) Lower bound @f$a@f$. Defaults to 0.
 %> @param  b     (optional) Upper bound @f$b@f$. Defaults to 1.
+%> @param  isEquilateral (optional) Transform to equilateral mesh.
 %> @retval g      A struct containing the lists that describe the
 %>                triangulation, as explained in <code>generateGridData()</code>.
 %>                <code>idE</code> and <code>ideE0T</code> are filled correctly.
 %>
 %> This file is part of FESTUNG
 %>
-%> @copyright 2014-2017 Florian Frank, Balthasar Reuter, Vadym Aizinger
+%> @copyright 2014-2018 Florian Frank, Balthasar Reuter, Vadym Aizinger
 %> @author Balthasar Reuter, 2017.
 %> 
 %> @par License
@@ -52,13 +60,19 @@
 %> along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %> @endparblock
 %
-function g = domainSquare(h, a, b)
+function g = domainSquare(h, a, b, isEquilateral)
 if nargin < 2, a = 0; end
 if nargin < 3, b = 1; end
 dim = ceil((b-a)/h); % number of edges per side of the unit square
 h = (b-a)/dim;
 %% Build coordV.
 [X, Y] = meshgrid(a:h:b);
+if nargin == 4 && isEquilateral == true % Transformation to become equilateral.
+  xTrans = repmat((0:h/2:(b-a)/2).', 1, dim+1);
+  X = X + xTrans;
+  yScale = sqrt(3)/2;
+  Y = (Y-(b-a))*yScale + (b-a);
+end
 Xlist = reshape(X, length(X)^2, 1);  Ylist = reshape(Y, length(X)^2, 1);
 coordV = [Xlist, Ylist];
 %% Build V0T.
